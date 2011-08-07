d4df566138eecfe893d51e68266e8702478018f8
jmsigner <jmsigner@38c049bd-fe20-d486-6346-6a8e9033181b>
2011-08-07 05:21:21 +0000

    sql fix

--- trunk/biodiversity_modelling/training/proj_slave.sh
+++ trunk/biodiversity_modelling/training/proj_slave.sh
@@ -10,7 +10,13 @@ runmodelid=$4
 
 # 2. mkdir and get files needed
 mkdir -p tnc_tmp/results/proj/$spid
-scp flora:tnc/results/$runmodelid/${spid:0:4}/$spid.zip tnc_tmp/results/proj/$spid
+if ! scp flora:tnc/results/$runmodelid/${spid:0:4}/$spid.zip tnc_tmp/results/proj/$spid
+then
+  ssh flora "echo 1 > tnc/tmp/${modelprojectid}.txt"
+  rm -r tnc_tmp/results/proj/$spid
+
+  exit
+fi
 
 unzip -d tnc_tmp/results/proj/$spid tnc_tmp/results/proj/$spid/$spid.zip
 
@@ -18,27 +24,29 @@ rm tnc_tmp/results/proj/$spid/$spid.zip
 
 if ! java -mx1024m -cp tnc_tmp/src/lib/maxent/maxent.jar density.Project tnc_tmp/results/proj/$spid/${spid}_0.lambdas tnc_tmp/data/env/proj/$env tnc_tmp/results/proj/$modelprojectid -a visible=false writeclampgrid=false warnings=false
 then
-  ssh flora "echo 1 > tnc/tmp/${modelprojectid}.txt"
+  ssh flora "echo 2 > tnc/tmp/${modelprojectid}.txt"
+  rm -r tnc_tmp/results/proj/$spid
   exit
 fi
         
 # rescale the reuslts
 if ! gdal_translate -scale 0 1 1 255 -ot byte -a_nodata 0 -q -co "COMPRESS=lzw" tnc_tmp/results/proj/$modelprojectid.asc tnc_tmp/results/proj/$modelprojectid.tif
 then 
-  ssh flora "echo 2 > tnc/tmp/${modelprojectid}.txt"
+  ssh flora "echo 3 > tnc/tmp/${modelprojectid}.txt"
+  rm -r tnc_tmp/results/proj/$spid
   exit
 fi
 
 # compress again to save an other 10%
 if ! gzip tnc_tmp/results/proj/$modelprojectid.tif
 then
-  ssh flora "echo 3 > tnc/tmp/${modelprojectid}.txt"
+  ssh flora "echo 4 > tnc/tmp/${modelprojectid}.txt"
   exit
 fi
 
 if ! scp tnc_tmp/results/proj/$modelprojectid.tif.gz flora:tnc/results/proj/
 then
-  ssh flora "echo 4 > tnc/tmp/${modelprojectid}.txt"
+  ssh flora "echo 5 > tnc/tmp/${modelprojectid}.txt"
   exit
 fi
 
