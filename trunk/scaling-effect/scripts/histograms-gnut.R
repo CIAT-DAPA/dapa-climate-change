7a27e859336ccf1d76c4851fb136a3b5ce83667d
dawnpatrolmustaine@gmail.com <dawnpatrolmustaine@gmail.com@38c049bd-fe20-d486-6346-6a8e9033181b>
2013-12-19 13:22:50 +0000

    add gnut analyses to scaling study

---
+++ trunk/scaling-effect/scripts/histograms-gnut.R
@@ -0,0 +1,115 @@
+#Julian Ramirez-Villegas
+#UoL / CCAFS / CIAT
+#Aug 2013
+stop("!")
+
+#load packages
+library(rgdal); library(raster); library(maptools); library(rasterVis); data(wrld_simpl)
+
+#source functions
+src.dir <- "~/Repositories/dapa-climate-change/trunk/scaling-effect"
+src.dir2 <- "~/Repositories/dapa-climate-change/trunk/PhD/0006-weather-data"
+source(paste(src.dir,"/scripts/EcoCrop-model.R",sep=""))
+source(paste(src.dir2,"/scripts/GHCND-GSOD-functions.R",sep=""))
+
+#i/o directories and details
+#bDir <- "/mnt/a102/eejarv/scaling-effect"
+#bDir <- "/nfs/a102/eejarv/scaling-effect"
+bDir <- "~/Leeds-work/scaling-effect"
+clmDir <- paste(bDir,"/climate_data",sep="")
+runDir <- paste(bDir,"/model-runs_gnut",sep="")
+lsmDir <- paste(bDir,"/lsm",sep="")
+#cascadeDir <- "/mnt/see-archive-12_a4scratch/eebjp"
+#cascadeDir <- "/nfs/see-archive-12_a4scratch/eebjp"
+
+#figure dir is local (on mbp)
+figDir <- paste(bDir,"/figures_gnut",sep="")
+if (!file.exists(figDir)) {dir.create(figDir)}
+
+#model run details
+trial <- 3
+crop_name <- "gnut"
+
+#get mask from CASCADE output
+msk <- raster(paste(lsmDir,"/Glam_12km_lsm.nc",sep=""))
+msk[which(msk[] < 0)] <- NA
+msk[which(msk[] > 0)] <- 1 #1:length(which(msk[] > 0))
+
+#find other interesting points
+msk2 <- raster(paste(lsmDir,"/3deg_mask.tif",sep=""))
+
+#new points
+s1 <- extent(-16.5,-13.5,12,15)
+s2 <- extent(7.5,10.5,12,15)
+
+p00 <- extent(msk)
+p00@ymax <- 15
+
+#load harvested area and locations on top
+ahar <- raster(paste(bDir,"/calendar/Groundnuts.crop.calendar/cascade_aharv.tif",sep=""))
+ahar[which(ahar[]==0)] <- NA; ahar[which(ahar[]>1)] <- 1
+ahar@crs <- wrld_simpl@proj4string
+
+#extract data
+extn <- extent(msk)
+extn@ymax <- 15
+msk2 <- crop(msk, extn)
+xy <- as.data.frame(xyFromCell(msk2,which(!is.na(msk2[]))))
+xy <- cbind(cell=cellFromXY(msk2,xy[,c("x","y")]),xy)
+
+#load area harvested and resample to climate grid
+aharv <- raster(paste(bDir,"/calendar/Groundnuts.crop.calendar/cascade_aharv.tif",sep=""))
+fac <- round(xres(msk2)/xres(aharv))
+aharv <- resample(aharv,msk2,method="ngb")
+xy$aharv <- extract(aharv, xy[,c("x","y")])
+
+#read 12km suitability raster
+resol <- "12km_exp"
+suit <- raster(paste(runDir,"/",resol,"/run_",trial,"/",crop_name,"_suitability.tif",sep=""))
+suit[which(suit[] == 0)] <- NA
+
+#make data.frame for constructing histogram
+his_df <- xy
+his_df$suit <- extract(suit,xy[,c("x","y")])
+his_df <- his_df[which(!is.na(his_df$suit)),]
+#his_df <- his_df[which(his_df$suit < 100 & his_df$suit > 0),]
+
+#construct histogram
+this <- hist(his_df$suit,breaks=seq(0,100,by=10),plot=F)
+this_01 <- hist(his_df$suit[which(his_df$aharv >= 0.1)],breaks=seq(0,100,by=10),plot=F)
+
+#read obs suitability raster
+suit_obs <- raster(paste(runDir,"/calib/run_",trial,"/",crop_name,"_suitability.tif",sep=""))
+suit_obs <- crop(suit_obs, msk2)
+suit_obs[which(suit_obs[] == 0)] <- NA
+
+aharv <- raster(paste(bDir,"/calendar/Groundnuts.crop.calendar/cascade_aharv.tif",sep=""))
+
+xy <- as.data.frame(xyFromCell(suit_obs,which(!is.na(suit_obs[]))))
+xy <- cbind(cell=cellFromXY(suit_obs,xy[,c("x","y")]),xy)
+xy$aharv <- extract(aharv, xy[,c("x","y")])
+
+his_dfo <- xy
+his_dfo$suit <- extract(suit_obs,xy[,c("x","y")])
+his_dfo <- his_dfo[which(!is.na(his_dfo$suit)),]
+#his_dfo <- his_dfo[which(his_dfo$suit < 100 & his_dfo$suit > 0),]
+
+#construct histogram
+this_o <- hist(his_dfo$suit,breaks=seq(0,100,by=10),plot=F)
+this_01o <- hist(his_dfo$suit[which(his_dfo$aharv >= 0.1)],breaks=seq(0,100,by=10),plot=F)
+
+#plot histograms
+pdf(paste(figDir,"/histograms.pdf",sep=""),width=10,height=7,pointsize=15)
+par(mar=c(4.5,4.5,1,1))
+plot(this$mids,this$counts/(sum(this$counts)),ty="l",col="red",xlim=c(0,100),ylim=c(0,1),
+     xlab="Suitability (%)",ylab="Relative frequency",lwd=2)
+lines(this_01$mids,this_01$counts/(sum(this_01$counts)),col="blue",lwd=2)
+lines(this_o$mids,this_o$counts/(sum(this_o$counts)),col="red",lty=2,lwd=2)
+lines(this_01o$mids,this_01o$counts/(sum(this_01o$counts)),col="blue",lty=2,lwd=2)
+grid(lwd=1.25)
+legend(x=0,y=1,cex=1.0,lty=c(1,1,2,2),col=c("red","blue","red","blue"),lwd=rep(1.5,4),
+       legend=c("All areas (CASCADE)","Harvested (CASCADE)", "All areas (Obs)", "Harvested (Obs)"))
+dev.off()
+
+
+
---
+++ trunk/scaling-effect/scripts/sites_plots-gnut.R
@@ -0,0 +1,204 @@
+#Julian Ramirez-Villegas
+#UoL / CCAFS / CIAT
+#Aug 2013
+stop("!")
+
+#load packages
+library(rgdal); library(raster); library(maptools); library(rasterVis); data(wrld_simpl)
+library(rasterVis)
+
+#source functions
+src.dir <- "~/Repositories/dapa-climate-change/trunk/scaling-effect"
+src.dir2 <- "~/Repositories/dapa-climate-change/trunk/PhD/0006-weather-data"
+source(paste(src.dir,"/scripts/EcoCrop-model.R",sep=""))
+source(paste(src.dir2,"/scripts/GHCND-GSOD-functions.R",sep=""))
+
+#i/o directories and details
+#bDir <- "/mnt/a102/eejarv/scaling-effect"
+#bDir <- "/nfs/a102/eejarv/scaling-effect"
+bDir <- "~/Leeds-work/scaling-effect"
+clmDir <- paste(bDir,"/climate_data",sep="")
+runDir <- paste(bDir,"/model-runs_gnut",sep="")
+lsmDir <- paste(bDir,"/lsm",sep="")
+#cascadeDir <- "/mnt/see-archive-12_a4scratch/eebjp"
+#cascadeDir <- "/nfs/see-archive-12_a4scratch/eebjp"
+
+#figure dir is local (on mbp)
+figDir <- paste(bDir,"/figures_gnut",sep="")
+if (!file.exists(figDir)) {dir.create(figDir)}
+
+#model run details
+trial <- 3
+crop_name <- "gnut"
+
+#get mask from CASCADE output
+msk <- raster(paste(lsmDir,"/Glam_12km_lsm.nc",sep=""))
+msk[which(msk[] < 0)] <- NA
+msk[which(msk[] > 0)] <- 1 #1:length(which(msk[] > 0))
+
+#find other interesting points
+if (!file.exists(paste(lsmDir,"/3deg_mask.tif",sep=""))) {
+  msk2 <- msk
+  msk2[which(!is.na(msk2[]))] <- rnorm(length(which(!is.na(msk2[]))),10,2)
+  writeRaster(msk2,paste(lsmDir),format="GTiff")
+} else {
+  msk2 <- raster(paste(lsmDir,"/3deg_mask.tif",sep=""))
+}
+
+#new points
+s1 <- extent(-16.5,-13.5,12,15)
+s2 <- extent(7.5,10.5,12,15)
+
+p00 <- extent(msk)
+p00@ymax <- 15
+
+#load harvested area and locations on top
+ahar <- raster(paste(bDir,"/calendar/Groundnuts.crop.calendar/cascade_aharv.tif",sep=""))
+ahar[which(ahar[]==0)] <- NA; ahar[which(ahar[]>1)] <- 1
+ahar@crs <- wrld_simpl@proj4string
+
+
+####
+#### plotting functions
+#functions to make plot
+rs_levplot2 <- function(rsin,zn,zx,nb,brks=NA,scale="YlOrRd",ncol=9,col_i="#CCECE6",col_f="#00441B",rev=F,leg=T) {
+  if (scale %in% row.names(brewer.pal.info)) {
+    pal <- rev(brewer.pal(ncol, scale))
+  } else {
+    pal <- colorRampPalette(c(col_i,col_f))(ncol)
+  }
+  if (rev) {pal <- rev(pal)}
+  
+  if (is.na(brks[1])) {brks <- do.breaks(c(zn,zx),nb)}
+  
+  #set theme
+  this_theme <- custom.theme(fill = pal,region = pal,
+                             bg = "white", fg = "grey20", pch = 14)
+  
+  p <- rasterVis:::levelplot(rsin, margin=F, par.settings = this_theme, colorkey=leg,
+                             at = brks, maxpixels=ncell(rsin)) + 
+    layer(sp.lines(grat,lwd=0.5,lty=2,col="grey 50")) +
+    layer(sp.polygons(wrld_simpl,lwd=0.8,col="black"))
+  return(p)
+}
+
+
+rs_print <- function(p,pdfName) {
+  pdf(pdfName,height=ht,width=wt,pointsize=14)
+  print(p)
+  dev.off()
+}
+
+#figure details
+ht <- 6
+rs <- msk
+fct <- (rs@extent@xmin-rs@extent@xmax)/(rs@extent@ymin-rs@extent@ymax)
+wt <- ht*(fct+.1)
+grat <- gridlines(wrld_simpl, easts=seq(-180,180,by=5), norths=seq(-90,90,by=5))
+
+
+#figure with locations
+pdf(paste(figDir,"/sites.pdf",sep=""), height=8,width=10,pointsize=14)
+tplot <- rs_levplot2(msk,0,1,nb=1,scale=NA,col_i="grey 80",col_f="grey 80",ncol=3,rev=F,leg=F)
+tplot <- tplot + layer(sp.polygons(as(s1,'SpatialPolygons'),lwd=1.8,col="blue"))
+tplot <- tplot + layer(panel.text((s1@xmin+s1@xmax)*.5, (s1@ymin+s1@ymax)*.5, "R1",cex=2))
+tplot <- tplot + layer(sp.polygons(as(s2,'SpatialPolygons'),lwd=1.8,col="blue"))
+tplot <- tplot + layer(panel.text((s2@xmin+s2@xmax)*.5, (s2@ymin+s2@ymax)*.5, "R2",cex=2))
+tplot <- tplot + layer(sp.polygons(as(p00,'SpatialPolygons'),lwd=3,col="red",lty=2))
+print(tplot)
+dev.off()
+
+
+#figure with harvested area and locations on top
+ahar <- raster(paste(bDir,"/calendar/Groundnuts.crop.calendar/cascade_aharv.tif",sep=""))
+ahar[which(ahar[]==0)] <- NA; ahar[which(ahar[]>1)] <- 1
+ahar@crs <- wrld_simpl@proj4string
+ahar <- resample(ahar,msk)
+
+#pdf(paste(figDir,"/sites_ahar_v2.pdf",sep=""), height=8,width=10,pointsize=14)
+#tplot <- rs_levplot2(ahar,zn=0,zx=1,nb=10,brks=c(0,0.02,0.04,0.06,.08,.1,.2,.6,.8,1,1.5),scale=NA,col_i="red",col_f="#FEE0D2",ncol=9,rev=T,leg=T)
+
+pdf(paste(figDir,"/sites_ahar.pdf",sep=""), height=8,width=10,pointsize=14)
+tplot <- rs_levplot2(ahar,zn=0,zx=1,nb=10,brks=NA,scale=NA,col_i="red",col_f="#FEE0D2",ncol=9,rev=T,leg=T)
+tplot <- tplot + layer(sp.polygons(as(s1,'SpatialPolygons'),lwd=1.8,col="blue"))
+tplot <- tplot + layer(panel.text((s1@xmin+s1@xmax)*.5, (s1@ymin+s1@ymax)*.5, "R1",cex=2))
+tplot <- tplot + layer(sp.polygons(as(s2,'SpatialPolygons'),lwd=1.8,col="blue"))
+tplot <- tplot + layer(panel.text((s2@xmin+s2@xmax)*.5, (s2@ymin+s2@ymax)*.5, "R2",cex=2))
+tplot <- tplot + layer(sp.polygons(as(p00,'SpatialPolygons'),lwd=3,col="red",lty=2))
+print(tplot)
+dev.off()
+
+
+###
+resol <- "12km_exp"
+trunDir <- paste(runDir,"/",resol,"/run_",trial,sep="")
+gsrain <- raster(paste(trunDir,"/",crop_name,"_gsrain.tif",sep=""))
+rsx <- gsrain
+
+rsx[which(rsx[] > 5000)] <- 5100
+rbrks <- c(seq(0,5000,by=250),5100)
+
+pdf(paste(figDir,"/gsrain_",resol,".pdf",sep=""), height=8,width=10,pointsize=14)
+tplot <- rs_levplot2(rsx,minval,maxval,nb=length(rbrks),brks=rbrks,scale=NA,
+                     col_i="#DEEBF7",col_f="#08306B",ncol=9,rev=F,leg=T)
+tplot <- tplot + layer(sp.polygons(as(s1,'SpatialPolygons'),lwd=1.8,col="blue"))
+tplot <- tplot + layer(panel.text((s1@xmin+s1@xmax)*.5, (s1@ymin+s1@ymax)*.5, "R1",cex=2))
+tplot <- tplot + layer(sp.polygons(as(s2,'SpatialPolygons'),lwd=1.8,col="blue"))
+tplot <- tplot + layer(panel.text((s2@xmin+s2@xmax)*.5, (s2@ymin+s2@ymax)*.5, "R2",cex=2))
+tplot <- tplot + layer(sp.polygons(as(p00,'SpatialPolygons'),lwd=3,col="red",lty=2))
+print(tplot)
+dev.off()
+
+
+#plot temperature data
+gstemp <- raster(paste(trunDir,"/",crop_name,"_gstmean.tif",sep=""))
+rsx <- gstemp*0.1
+minval <- min(gstemp[],na.rm=T)
+maxval <- max(gstemp[],na.rm=T)
+
+tbrks <- seq(10,45,by=2.5)
+
+pdf(paste(figDir,"/gstemp_",resol,".pdf",sep=""), height=8,width=10,pointsize=14)
+tplot <- rs_levplot2(rsx,minval,maxval,nb=length(tbrks),brks=tbrks,scale="YlOrRd",ncol=9,rev=T,leg=T)
+tplot <- tplot + layer(sp.polygons(as(s1,'SpatialPolygons'),lwd=1.8,col="blue"))
+tplot <- tplot + layer(panel.text((s1@xmin+s1@xmax)*.5, (s1@ymin+s1@ymax)*.5, "R1",cex=2))
+tplot <- tplot + layer(sp.polygons(as(s2,'SpatialPolygons'),lwd=1.8,col="blue"))
+tplot <- tplot + layer(panel.text((s2@xmin+s2@xmax)*.5, (s2@ymin+s2@ymax)*.5, "R2",cex=2))
+tplot <- tplot + layer(sp.polygons(as(p00,'SpatialPolygons'),lwd=3,col="red",lty=2))
+print(tplot)
+dev.off()
+
+
+
+#plot the worldclim ones
+gsrain <- raster(paste(runDir,"/calib/run_",trial,"/",crop_name,"_gsrain.tif",sep=""))
+gsrain <- crop(gsrain,msk)
+gsrain <- resample(gsrain,rsx)
+rsx <- gsrain
+
+pdf(paste(figDir,"/gsrain_wcl.pdf",sep=""), height=8,width=10,pointsize=14)
+tplot <- rs_levplot2(rsx,minval,maxval,nb=length(rbrks),brks=rbrks,scale=NA,
+                     col_i="#DEEBF7",col_f="#08306B",ncol=9,rev=F,leg=T)
+tplot <- tplot + layer(sp.polygons(as(s1,'SpatialPolygons'),lwd=1.8,col="blue"))
+tplot <- tplot + layer(panel.text((s1@xmin+s1@xmax)*.5, (s1@ymin+s1@ymax)*.5, "R1",cex=2))
+tplot <- tplot + layer(sp.polygons(as(s2,'SpatialPolygons'),lwd=1.8,col="blue"))
+tplot <- tplot + layer(panel.text((s2@xmin+s2@xmax)*.5, (s2@ymin+s2@ymax)*.5, "R2",cex=2))
+tplot <- tplot + layer(sp.polygons(as(p00,'SpatialPolygons'),lwd=3,col="red",lty=2))
+print(tplot)
+dev.off()
+
+
+gstemp <- raster(paste(runDir,"/calib/run_",trial,"/",crop_name,"_gstmean.tif",sep=""))
+gstemp <- crop(gstemp,msk)
+gstemp <- resample(gstemp,rsx)
+rsx <- gstemp*0.1
+
+pdf(paste(figDir,"/gstemp_wcl.pdf",sep=""), height=8,width=10,pointsize=14)
+tplot <- rs_levplot2(rsx,minval,maxval,nb=length(tbrks),brks=tbrks,scale="YlOrRd",ncol=9,rev=T,leg=T)
+tplot <- tplot + layer(sp.polygons(as(s1,'SpatialPolygons'),lwd=1.8,col="blue"))
+tplot <- tplot + layer(panel.text((s1@xmin+s1@xmax)*.5, (s1@ymin+s1@ymax)*.5, "R1",cex=2))
+tplot <- tplot + layer(sp.polygons(as(s2,'SpatialPolygons'),lwd=1.8,col="blue"))
+tplot <- tplot + layer(panel.text((s2@xmin+s2@xmax)*.5, (s2@ymin+s2@ymax)*.5, "R2",cex=2))
+tplot <- tplot + layer(sp.polygons(as(p00,'SpatialPolygons'),lwd=3,col="red",lty=2))
+print(tplot)
+dev.off()
