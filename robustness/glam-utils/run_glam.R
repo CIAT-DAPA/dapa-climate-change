8db9d1a673e12e01e6c09cf4553f50df82bf2753
dawnpatrolmustaine <dawnpatrolmustaine@gmail.com>
2014-02-14 02:18:03 +0000

    add run_glam.R to robustness study

---
+++ robustness/glam-utils/run_glam.R
@@ -0,0 +1,13 @@
+#Julian Ramirez-Villegas
+#UoL / CCAFS
+#Feb 2014
+
+#single run of GLAM for a given set of inputs. This run will
+#1. create directories
+#2. write meteorology, soil and sowing file
+#3. write parameter file
+#4. write filenames file
+
+src.dir <- "~/Repositories/dapa-climate-change/trunk/robustness"
+source(paste(src.dir,"/glam-utils/make_dirs.R",sep=""))
+source()
