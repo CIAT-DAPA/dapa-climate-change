5ca7e1bc4c6ccf766849e791f714eb2278003c8e
dawnpatrolmustaine <dawnpatrolmustaine@gmail.com>
2014-02-23 00:04:31 +0000

    update scaling scripts

---
+++ scaling-effect/scripts/paper_figures/si_fig_03.R
@@ -0,0 +1,137 @@
+#Julian Ramirez-Villegas
+#UoL / CCAFS / CIAT
+#Aug 2013
+stop("!")
+
+#load packages
+library(rgdal); library(raster); library(maptools); library(rasterVis); data(wrld_simpl)
+
+#source functions
+src.dir <- "~/Repositories/dapa-climate-change/trunk/scaling-effect"
+src.dir2 <- "~/Repositories/dapa-climate-change/trunk/PhD/0006-weather-data"
+source(paste(src.dir,"/scripts/EcoCrop-model.R",sep=""))
+source(paste(src.dir2,"/scripts/GHCND-GSOD-functions.R",sep=""))
+
+#i/o directories and details
+#bDir <- "/mnt/a102/eejarv/scaling-effect"
+#bDir <- "/nfs/a102/eejarv/scaling-effect"
+bDir <- "~/Leeds-work/scaling-effect"
+clmDir <- paste(bDir,"/climate_data",sep="")
+lsmDir <- paste(bDir,"/lsm",sep="")
+#cascadeDir <- "/mnt/see-archive-12_a4scratch/eebjp"
+#cascadeDir <- "/nfs/see-archive-12_a4scratch/eebjp"
+
+#figure dir is local (on mbp)
+figDir <- paste(bDir,"/paper_figures",sep="")
+
+################################################################################################
+########## for maize
+#model run details
+runDir <- paste(bDir,"/model-runs",sep="")
+trial <- 6
+crop_name <- "maiz"
+
+#get mask from CASCADE output
+msk <- raster(paste(lsmDir,"/Glam_12km_lsm.nc",sep=""))
+msk[which(msk[] < 0)] <- NA
+msk[which(msk[] > 0)] <- 1 #1:length(which(msk[] > 0))
+
+#load harvested area and locations on top
+ahar <- raster(paste(bDir,"/calendar/Maize.crop.calendar/cascade_aharv.tif",sep=""))
+ahar[which(ahar[]==0)] <- NA; ahar[which(ahar[]>1)] <- 1
+ahar@crs <- wrld_simpl@proj4string
+
+#extract data
+extn <- extent(msk)
+extn@ymax <- 15
+msk2 <- crop(msk, extn)
+xy <- as.data.frame(xyFromCell(msk2,which(!is.na(msk2[]))))
+xy <- cbind(cell=cellFromXY(msk2,xy[,c("x","y")]),xy)
+
+#load area harvested and resample to climate grid
+aharv <- raster(paste(bDir,"/calendar/Maize.crop.calendar/cascade_aharv.tif",sep=""))
+fac <- round(xres(msk2)/xres(aharv))
+aharv <- resample(aharv,msk2,method="ngb")
+xy$aharv <- extract(aharv, xy[,c("x","y")])
+
+#read 12km suitability raster
+resol <- "12km_exp"
+suit <- raster(paste(runDir,"/",resol,"/run_",trial,"/",crop_name,"_suitability.tif",sep=""))
+suit[which(suit[] == 0)] <- NA
+
+#make data.frame for constructing histogram
+his_df <- xy
+his_df$suit <- extract(suit,xy[,c("x","y")])
+his_df <- his_df[which(!is.na(his_df$suit)),]
+
+#construct histogram
+this_all <- hist(his_df$suit,breaks=seq(0,100,by=10),plot=F)
+this_niche <- hist(his_df$suit[which(his_df$aharv >= 0.1)],breaks=seq(0,100,by=10),plot=F)
+
+#plot histograms
+pdf(paste(figDir,"/SI-Fig03a_maize.pdf",sep=""),width=8,height=5,pointsize=12)
+par(mar=c(4.5,4.5,1,1))
+plot(this_all$mids,this_all$counts/(sum(this_all$counts)),ty="l",col="black",xlim=c(0,100),ylim=c(0,1),
+     xlab="Suitability (%)",ylab="Relative frequency",lwd=2)
+lines(this_niche$mids,this_niche$counts/(sum(this_niche$counts)),col="blue",lwd=2)
+grid(lwd=1.25)
+abline(v=mean(his_df$suit),col="black",lty=2)
+abline(v=mean(his_df$suit[which(his_df$aharv >= 0.05)]),col="blue",lty=2)
+legend(x=0,y=1,cex=1.0,lty=c(1,1),col=c("black","blue"),lwd=rep(1.5,2),
+       legend=c("All areas","Niche"))
+dev.off()
+
+
+##############################################################################
+### for groundnut
+runDir <- paste(bDir,"/model-runs_gnut",sep="")
+trial <- 3
+crop_name <- "gnut"
+
+#load harvested area and locations on top
+ahar <- raster(paste(bDir,"/calendar/Groundnuts.crop.calendar/cascade_aharv.tif",sep=""))
+ahar[which(ahar[]==0)] <- NA; ahar[which(ahar[]>1)] <- 1
+ahar@crs <- wrld_simpl@proj4string
+
+#extract data
+extn <- extent(msk)
+extn@ymax <- 15
+msk2 <- crop(msk, extn)
+xy <- as.data.frame(xyFromCell(msk2,which(!is.na(msk2[]))))
+xy <- cbind(cell=cellFromXY(msk2,xy[,c("x","y")]),xy)
+
+#load area harvested and resample to climate grid
+aharv <- raster(paste(bDir,"/calendar/Groundnuts.crop.calendar/cascade_aharv.tif",sep=""))
+fac <- round(xres(msk2)/xres(aharv))
+aharv <- resample(aharv,msk2,method="ngb")
+xy$aharv <- extract(aharv, xy[,c("x","y")])
+
+#read 12km suitability raster
+resol <- "12km_exp"
+suit <- raster(paste(runDir,"/",resol,"/run_",trial,"/",crop_name,"_suitability.tif",sep=""))
+suit[which(suit[] == 0)] <- NA
+
+#make data.frame for constructing histogram
+his_df <- xy
+his_df$suit <- extract(suit,xy[,c("x","y")])
+his_df <- his_df[which(!is.na(his_df$suit)),]
+
+#construct histogram
+this_all <- hist(his_df$suit,breaks=seq(0,100,by=10),plot=F)
+this_niche <- hist(his_df$suit[which(his_df$aharv >= 0.05)],breaks=seq(0,100,by=10),plot=F)
+
+#plot histograms
+pdf(paste(figDir,"/SI-Fig03b_groundnut.pdf",sep=""),width=8,height=5,pointsize=12)
+par(mar=c(4.5,4.5,1,1))
+plot(this_all$mids,this_all$counts/(sum(this_all$counts)),ty="l",col="black",xlim=c(0,100),ylim=c(0,1),
+     xlab="Suitability (%)",ylab="Relative frequency",lwd=2)
+lines(this_niche$mids,this_niche$counts/(sum(this_niche$counts)),col="blue",lwd=2)
+grid(lwd=1.25)
+abline(v=mean(his_df$suit),col="black",lty=2)
+abline(v=mean(his_df$suit[which(his_df$aharv >= 0.05)]),col="blue",lty=2)
+legend(x=0,y=1,cex=1.0,lty=c(1,1),col=c("black","blue"),lwd=rep(1.5,2),
+       legend=c("All areas","Niche"))
+dev.off()
+
+
+
---
+++ scaling-effect/scripts/paper_figures/si_fig_05.R
@@ -0,0 +1,85 @@
+#Julian Ramirez-Villegas
+#Feb 2012
+#UoL / CCAFS / CIAT
+
+###
+#plot EcoCrop boxplots for lines in Fig. 2: 12km, 12km-niche, 3-deg
+###
+
+#load packages
+library(rgdal); library(raster); library(maptools); library(rasterVis); data(wrld_simpl)
+library(ggplot2); library(reshape2)
+
+#source functions
+src.dir <- "~/Repositories/dapa-climate-change/trunk/scaling-effect"
+src.dir2 <- "~/Repositories/dapa-climate-change/trunk/PhD/0006-weather-data"
+source(paste(src.dir,"/scripts/EcoCrop-model.R",sep=""))
+source(paste(src.dir2,"/scripts/GHCND-GSOD-functions.R",sep=""))
+
+#i/o directories and details
+#bDir <- "/mnt/a102/eejarv/scaling-effect"
+#bDir <- "/nfs/a102/eejarv/scaling-effect"
+bDir <- "~/Leeds-work/scaling-effect"
+clmDir <- paste(bDir,"/climate_data",sep="")
+runDir <- paste(bDir,"/model-runs",sep="")
+lsmDir <- paste(bDir,"/lsm",sep="")
+#cascadeDir <- "/mnt/see-archive-12_a4scratch/eebjp"
+#cascadeDir <- "/nfs/see-archive-12_a4scratch/eebjp"
+
+sensDir_12km <- paste(runDir,"/sens",sep="")
+sensDir_3d12 <- paste(runDir,"/sens_3deg-12km_exp",sep="")
+
+#figure dir is local (on mbp)
+figDir <- paste(bDir,"/paper_figures",sep="")
+
+#make sensitivity table
+sensruns <- expand.grid(TEMP=seq(-1,6,by=1),PREC=seq(-0.9,0.2,by=0.1))
+
+#read sensitivity results
+outsens_12km <- read.csv(paste(sensDir_12km,"/sensitivity_result_raw.csv",sep=""))
+outsens_3d12 <- read.csv(paste(sensDir_3d12,"/sensitivity_result_raw.csv",sep=""))
+
+#sub-select only dT between 0 and 5
+outsens_12km <- outsens_12km[which(outsens_12km$temp >= 0 & outsens_12km$temp <= 5),]
+outsens_12km_all <- outsens_12km[which(outsens_12km$prec == 0 & outsens_12km$type == "all"),]
+outsens_12km_har <- outsens_12km[which(outsens_12km$prec == 0 & outsens_12km$type == "har"),]
+
+outsens_3d12 <- outsens_3d12[which(outsens_3d12$temp >= 0 & outsens_3d12$temp <= 5),]
+outsens_3d12_all <- outsens_3d12[which(outsens_3d12$prec == 0 & outsens_3d12$type == "all"),]
+outsens_3d12_har <- outsens_3d12[which(outsens_3d12$prec == 0 & outsens_3d12$type == "har"),]
+
+pdf(paste(figDir,"/SI-Fig05d_ecocrop_12km.pdf",sep=""), height=6,width=8,pointsize=12)
+par(mar=c(5,5,1,1),las=1,lwd=1.25)
+boxplot(outsens_12km_all$diff~outsens_12km_all$temp,pch=3,cex=0.5,col="white",border="black",
+        ylim=c(-100,0),xlab="Temperature change (K)", ylab="Suitability change (%)",
+        outcol="red",medcol="red",boxcol="blue")
+grid()
+boxplot(outsens_12km_all$diff~outsens_12km_all$temp,pch=3,cex=0.5,col="white",border="black",
+        ylim=c(-100,0),xlab="Temperature change (K)", ylab="Suitability change (%)",
+        outcol="red",medcol="red",boxcol="blue",add=T)
+dev.off()
+
+pdf(paste(figDir,"/SI-Fig05e_ecocrop_12km_niche.pdf",sep=""), height=6,width=8,pointsize=12)
+par(mar=c(5,5,1,1),las=1,lwd=1.25)
+boxplot(outsens_12km_har$diff~outsens_12km_har$temp,pch=3,cex=0.5,col="white",border="black",
+        ylim=c(-100,0),xlab="Temperature change (K)", ylab="Suitability change (%)",
+        outcol="red",medcol="red",boxcol="blue")
+grid()
+boxplot(outsens_12km_har$diff~outsens_12km_har$temp,pch=3,cex=0.5,col="white",border="black",
+        ylim=c(-100,0),xlab="Temperature change (K)", ylab="Suitability change (%)",
+        outcol="red",medcol="red",boxcol="blue",add=T)
+dev.off()
+
+
+pdf(paste(figDir,"/SI-Fig05f_ecocrop_3deg.pdf",sep=""), height=6,width=8,pointsize=12)
+par(mar=c(5,5,1,1),las=1,lwd=1.25)
+boxplot(outsens_3d12_all$diff~outsens_3d12_all$temp,pch=3,cex=0.5,col="white",border="black",
+        ylim=c(-100,0),xlab="Temperature change (K)", ylab="Suitability change (%)",
+        outcol="red",medcol="red",boxcol="blue")
+grid()
+boxplot(outsens_3d12_all$diff~outsens_3d12_all$temp,pch=3,cex=0.5,col="white",border="black",
+        ylim=c(-100,0),xlab="Temperature change (K)", ylab="Suitability change (%)",
+        outcol="red",medcol="red",boxcol="blue",add=T)
+dev.off()
+
+
