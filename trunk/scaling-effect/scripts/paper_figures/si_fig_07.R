486bf1318a120d99370845bc243993acc660ef0a
dawnpatrolmustaine@gmail.com <dawnpatrolmustaine@gmail.com@38c049bd-fe20-d486-6346-6a8e9033181b>
2014-03-03 19:40:13 +0000

    updated scaling scripts

--- trunk/scaling-effect/scripts/paper_figures/si_fig_07.R
+++ trunk/scaling-effect/scripts/paper_figures/si_fig_07.R
@@ -45,7 +45,7 @@ p <- p + theme(legend.key.height=unit(2.75,"cm"),legend.key.width=unit(1,"cm"),
                axis.title=element_text(colour="black",size=15,face="bold"))
 p <- p + labs(x = "Temperature change (K)", y = "Precipitation change (%)")
 
-pdf(paste(figDir,"/SI-Fig07c_ecocrop_all.pdf",sep=""), height=6,width=8,pointsize=12,family="Helvetica")
+pdf(paste(figDir,"/SI-Fig07d_ecocrop_12km_all.pdf",sep=""), height=6,width=8,pointsize=12,family="Helvetica")
 print(p)
 dev.off()
 
@@ -65,7 +65,35 @@ p <- p + theme(legend.key.height=unit(2.75,"cm"),legend.key.width=unit(1,"cm"),
                axis.title=element_text(colour="black",size=15,face="bold"))
 p <- p + labs(x = "Temperature change (K)", y = "Precipitation change (%)")
 
-pdf(paste(figDir,"/SI-Fig07d_ecocrop_niche.pdf",sep=""), height=6,width=8,pointsize=12,family="Helvetica")
+pdf(paste(figDir,"/SI-Fig07e_ecocrop_12km_niche.pdf",sep=""), height=6,width=8,pointsize=12,family="Helvetica")
+print(p)
+dev.off()
+
+
+###
+#3 deg heatmap
+sensDir <- paste(runDir,"/sens_3deg-12km_exp",sep="")
+
+#read in sensitivity output
+outsens <- read.csv(paste(sensDir,"/sensitivity_result.csv",sep=""))
+outsens <- outsens[which(outsens$temp >= 0 & outsens$temp <= 5),]
+
+#heatmap of sensitivity of 3deg runs
+hplot_df <- outsens[,c("prec","temp","reldiff_all")]
+hplot_df$prec <- as.factor(hplot_df$prec*100)
+hplot_df$temp <- as.factor(hplot_df$temp)
+
+p <- ggplot(data=hplot_df, aes(temp, prec)) + geom_tile(aes(fill = reldiff_all), colour = NA)
+p <- p + scale_fill_gradient2(name="", low = "red", mid="white", high = "blue", 
+                              midpoint=0, limits=c(-100,100),guide="colourbar")
+p <- p + theme(legend.key.height=unit(2.75,"cm"),legend.key.width=unit(1,"cm"),
+               legend.text=element_text(size=13),
+               panel.background=element_rect(fill="white",colour="black"),
+               axis.text=element_text(colour="black",size=13),
+               axis.title=element_text(colour="black",size=15,face="bold"))
+p <- p + labs(x = "Temperature change (K)", y = "Precipitation change (%)")
+
+pdf(paste(figDir,"/SI-Fig07f_ecocrop_3deg_all.pdf",sep=""), height=6,width=8,pointsize=12,family="Helvetica")
 print(p)
 dev.off()
 
