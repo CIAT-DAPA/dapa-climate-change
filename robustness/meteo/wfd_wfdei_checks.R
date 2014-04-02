09345d4b685c1d48c902701479ac35bd2b3ee844
dawnpatrolmustaine <dawnpatrolmustaine@gmail.com>
2014-04-02 21:33:15 +0000

    update robustness scripts

---
+++ robustness/meteo/wfd_wfdei_checks.R
@@ -0,0 +1,377 @@
+#Julian Ramirez-Villegas
+#UoL / CCAFS / CIAT
+#Apr 2014
+
+#############################################################################################
+####### WFD/WFDEI data looked dodgy in places so i decided to compare these to CRU and WCL
+#############################################################################################
+
+#packages
+library(raster); library(rgdal); library(ncdf)
+
+#input directories
+wd <- "~/Leeds-work/quest-for-robustness"
+mdata_dir <- paste(wd,"/data/model_data",sep="")
+met_dir <- paste(wd,"/data/meteorology",sep="")
+yi_dir <- paste(wd,"/data/yield_data_maize",sep="")
+
+#load objects
+load(paste(mdata_dir,"/initial_conditions_major.RData",sep=""))
+load(paste(mdata_dir,"/yield_major.RData",sep=""))
+
+#define dataset, period, variable
+dataset <- "WFD" #WFD, WFDEI
+years <- 1982:2001 #1982:2001 for WFD, 1982:2005 for WFDEI
+
+#loop variables
+for (vname in c("Rainf","SWdown","Tmax","Tmin")) {
+  #vname <- "Rainf"
+  if (vname == "Rainf") {suffix <- "_GPCC"} else {suffix <- ""}
+  
+  cat("\n...processing",vname,"\n")
+  
+  #i/o data dirs
+  data_dir <- paste(met_dir,"/baseline_climate/",vname,"_daily_",dataset,suffix,sep="")
+  mdata_odir <- paste(met_dir,"/baseline_climate/",vname,"_monthly_",dataset,suffix,sep="")
+  if (!file.exists(mdata_odir)) {dir.create(mdata_odir)}
+  ydata_odir <- paste(met_dir,"/baseline_climate/",vname,"_yearly_",dataset,suffix,sep="")
+  if (!file.exists(ydata_odir)) {dir.create(ydata_odir)}
+  cdata_odir <- paste(met_dir,"/baseline_climate/",vname,"_climatology_",dataset,suffix,sep="")
+  if (!file.exists(cdata_odir)) {dir.create(cdata_odir)}
+  
+  #mean climate output file
+  clm_fname <- paste(cdata_odir,"/afr_",vname,"_meanclim_",dataset,suffix,"_",min(years),"-",max(years),".tif",sep="")
+  
+  #variability (sigma) output file
+  var_fname <- paste(cdata_odir,"/afr_",vname,"_stdclim_",dataset,suffix,"_",min(years),"-",max(years),".tif",sep="")
+  
+  #variability (c.v.) output file
+  cv_fname <- paste(cdata_odir,"/afr_",vname,"_cvclim_",dataset,suffix,"_",min(years),"-",max(years),".tif",sep="")
+  
+  #loop years
+  for (yr in years) {
+    #yr <- 1982
+    cat("\n...processing year=",yr,"\n")
+    
+    #name of yearly file
+    y_fname <- paste(ydata_odir,"/afr_",vname,"_yearly_",dataset,suffix,"_",yr,".tif",sep="")
+    
+    if (!file.exists(y_fname)) {
+      #loop months
+      for (m in 1:12) {
+        #m <- 1
+        cat("...processing month=",m,"\n")
+        
+        #file names
+        i_fname <- paste(data_dir,"/afr_",vname,"_daily_",dataset,suffix,"_",yr,sprintf("%1$02d",m),".nc",sep="")
+        m_fname <- paste(mdata_odir,"/afr_",vname,"_monthly_",dataset,suffix,"_",yr,sprintf("%1$02d",m),".tif",sep="")
+        
+        if (!file.exists(m_fname)) {
+          #read raster
+          rs <- stack(i_fname)
+          
+          #convert units
+          if (vname == "Rainf") {
+            rs <- rs * 3600 * 24 #kg m-2 s-1 to mm/day
+          } else if (vname == "SWdown") {
+            rs <- rs * 24 * 3600 / 1000000 #w/m2/s = J/m2/s / 1000000 * 86400 = MJ/m2/day
+          } else {
+            rs <- rs - 273.15 #K to C
+          }
+          
+          #sum or mean
+          if (vname == "Rainf") {
+            rs_m <- calc(rs, function(x) {sum(x,na.rm=T)})
+          } else {
+            rs_m <- calc(rs, function(x) {mean(x,na.rm=T)})
+          }
+          
+          #write raster
+          rs_m <- writeRaster(rs_m,m_fname,format="GTiff")
+        } else {
+          rs_m <- raster(m_fname)
+        }
+        
+        #put everything into a list/stack
+        if (m == 1) {rs_y <- rs_m} else {rs_y <- c(rs_y,rs_m)}
+      }
+      
+      #calculate mean / total for year
+      rs_y <- stack(rs_y)
+      #sum or mean
+      if (vname == "Rainf") {
+        rs_y <- calc(rs_y, function(x) {sum(x,na.rm=T)})
+      } else {
+        rs_y <- calc(rs_y, function(x) {mean(x,na.rm=T)})
+      }
+      
+      #write raster
+      rs_y <- writeRaster(rs_y,y_fname,format="GTiff")
+    } else {
+      rs_y <- raster(y_fname)
+    }
+    
+    if (yr == years[1]) {
+      rs_clm <- rs_y
+    } else {
+      rs_clm <- c(rs_clm,rs_y)
+    }
+  }
+  
+  rs_clm <- stack(rs_clm)
+  
+  #mean
+  if (!file.exists(clm_fname)) {
+    rs_mean <- calc(rs_clm, function(x) {mean(x,na.rm=T)})
+    rs_mean <- writeRaster(rs_mean,clm_fname,format="GTiff")
+  } else {
+    rs_mean <- raster(clm_fname)
+  }
+  
+  #s.d.
+  if (!file.exists(var_fname)) {
+    rs_var <- calc(rs_clm, function(x) {sd(x,na.rm=T)})
+    rs_var <- writeRaster(rs_var,var_fname,format="GTiff")
+  } else {
+    rs_var <- raster(var_fname)
+  }
+  
+  #c.v.
+  if (!file.exists(cv_fname)) {
+    if (vname == "Rainf") {
+      rs_cv <- rs_var / (rs_mean+0.1) * 100
+    } else {
+      rs_cv <- rs_var / (rs_mean) * 100
+    }
+    rs_cv <- writeRaster(rs_cv,cv_fname,format="GTiff")
+  }
+  
+  #calculate climatological mean, s.d., c.v. for all months
+  cat("\n... calculating monthly means\n")
+  for (m in 1:12) {
+    #m <- 1
+    cat("...processing month=",m,"\n")
+    
+    cmth_fname_clm <- paste(cdata_odir,"/afr_",vname,"_meanclim_",dataset,suffix,"_",min(years),"-",max(years),"_",sprintf("%1$02d",m),".tif",sep="")
+    cmth_fname_var <- paste(cdata_odir,"/afr_",vname,"_stdclim_",dataset,suffix,"_",min(years),"-",max(years),"_",sprintf("%1$02d",m),".tif",sep="")
+    cmth_fname_cv <- paste(cdata_odir,"/afr_",vname,"_cvclim_",dataset,suffix,"_",min(years),"-",max(years),"_",sprintf("%1$02d",m),".tif",sep="")
+    
+    #load all years rasters
+    rs_stk <- stack(paste(mdata_odir,"/afr_",vname,"_monthly_",dataset,suffix,"_",years,sprintf("%1$02d",m),".tif",sep=""))
+    
+    #mean
+    if (!file.exists(cmth_fname_clm)) {
+      rs_mmean <- calc(rs_stk, function(x) {mean(x,na.rm=T)})
+      rs_mmean <- writeRaster(rs_mmean,cmth_fname_clm,format="GTiff")
+    } else {
+      rs_mmean <- raster(cmth_fname_clm)
+    }
+    
+    #s.d.
+    if (!file.exists(cmth_fname_var)) {
+      rs_mvar <- calc(rs_stk, function(x) {sd(x,na.rm=T)})
+      rs_mvar <- writeRaster(rs_mvar,cmth_fname_var,format="GTiff")
+    } else {
+      rs_mvar <- raster(cmth_fname_var)
+    }
+    
+    #c.v.
+    if (!file.exists(cmth_fname_cv)) {
+      if (vname == "Rainf") {
+        rs_mcv <- rs_mvar / (rs_mmean+0.1) * 100
+      } else {
+        rs_mcv <- rs_mvar / (rs_mmean) * 100
+      }
+      rs_mcv <- writeRaster(rs_cv,cmth_fname_cv,format="GTiff")
+    }
+  }
+}
+
+
+################################################################################
+################################################################################
+#### calculate mean, s.d. and c.v. for cru, for africa
+
+cru_idir <- "~/Leeds-work/datasets/meteorology/cru-ts-v3-21"
+cru_odir <- paste(met_dir,"/cru-ts-v3-21",sep="")
+if (!file.exists(cru_odir)) {dir.create(cru_odir)}
+
+#determine extent to cut the resampled netcdf
+yrs <- raster(paste(yi_dir,"/descriptive_stats/mean_ModelYld500.tif",sep=""))
+bbox <- extent(yrs)
+
+##first process the CRU time series 1950-2012 (change resolution and cut to Africa)
+#loop variables
+for (vname in c("pre","tmn","tmx","tmp")) {
+  #vname <- "pre"
+  cat("\n...processing variable=",vname,"\n")
+  
+  cru_ovdir <- paste(cru_odir,"/",vname,sep="")
+  if (!file.exists(cru_ovdir)) {dir.create(cru_ovdir)}
+  
+  #loop years
+  for (yr in 1950:2012) {
+    #yr <- 1950
+    cat("...processing cru for year=",yr,"\n")
+    
+    ifil <- paste(cru_idir,"/",vname,"/",vname,"_",yr,".nc",sep="")
+    ofil <- paste(cru_ovdir,"/afr_",vname,"_",yr,".nc",sep="")
+    
+    if (!file.exists(ofil)) {
+      #first remapcon2
+      system(paste("cdo remapcon2,r320x160 ",ifil," ",cru_ovdir,"/",vname,"_remapped.nc",sep=""))
+      
+      #second cut to africa
+      system(paste("cdo sellonlatbox,",bbox@xmin,",",bbox@xmax,",",bbox@ymin,",",bbox@ymax," ",cru_ovdir,"/",vname,"_remapped.nc ",ofil,sep=""))
+      
+      #remove junk
+      system(paste("rm -f ",cru_ovdir,"/",vname,"_remapped.nc",sep=""))
+    }
+  }
+}
+
+
+###
+#calculate sum / mean of year, climatology for year and months
+for (vname in c("pre","tmn","tmx","tmp")) {
+  #vname <- "pre"
+  cru_ovdir <- paste(cru_odir,"/",vname,sep="")
+  yr_cru_odir <- paste(cru_odir,"/",vname,"_yearly",sep="")
+  if (!file.exists(yr_cru_odir)) {dir.create(yr_cru_odir)}
+  clm_cru_odir <- paste(cru_odir,"/",vname,"_climatology",sep="")
+  if (!file.exists(clm_cru_odir)) {dir.create(clm_cru_odir)}
+  
+  #loop years
+  for (yr in years) {
+    #yr <- years[1]
+    cat("...processing year=",yr,"\n")
+    
+    y_fname <- paste(yr_cru_odir,"/afr_",vname,"_",yr,".tif",sep="")
+    
+    if (!file.exists(y_fname)) {
+      rs_y <- stack(paste(cru_ovdir,"/afr_",vname,"_",yr,".nc",sep=""))
+      if (vname == "pre") {
+        rs_y <- calc(rs_y, function(x) {sum(x,na.rm=T)})
+      } else {
+        rs_y <- calc(rs_y, function(x) {mean(x,na.rm=T)})
+      }
+      rs_y <- writeRaster(rs_y,y_fname,format="GTiff")
+    } else {
+      rs_y <- raster(y_fname)
+    }
+    
+    if (yr == years[1]) {
+      rs_clm <- rs_y
+    } else {
+      rs_clm <- c(rs_clm,rs_y)
+    }
+  }
+  
+  rs_clm <- stack(rs_clm)
+  
+  clm_fname <- paste(clm_cru_odir,"/afr_",vname,"_meanclim_",min(years),"-",max(years),".tif",sep="")
+  var_fname <- paste(clm_cru_odir,"/afr_",vname,"_stdclim_",min(years),"-",max(years),".tif",sep="")
+  cv_fname <- paste(clm_cru_odir,"/afr_",vname,"_cvclim_",min(years),"-",max(years),".tif",sep="")
+  
+  #mean
+  if (!file.exists(clm_fname)) {
+    rs_mean <- calc(rs_clm, function(x) {mean(x,na.rm=T)})
+    rs_mean <- writeRaster(rs_mean,clm_fname,format="GTiff")
+  } else {
+    rs_mean <- raster(clm_fname)
+  }
+  
+  #s.d.
+  if (!file.exists(var_fname)) {
+    rs_var <- calc(rs_clm, function(x) {sd(x,na.rm=T)})
+    rs_var <- writeRaster(rs_var,var_fname,format="GTiff")
+  } else {
+    rs_var <- raster(var_fname)
+  }
+  
+  #c.v.
+  if (!file.exists(cv_fname)) {
+    if (vname == "pre") {
+      rs_cv <- rs_var / (rs_mean+0.1) * 100
+    } else {
+      rs_cv <- rs_var / (rs_mean) * 100
+    }
+    rs_cv <- writeRaster(rs_cv,cv_fname,format="GTiff")
+  }
+  
+  #calculate climatological mean, s.d., c.v. for all months
+  cat("\n... calculating monthly means\n")
+  for (m in 1:12) {
+    #m <- 1
+    cat("...processing month=",m,"\n")
+    
+    cmth_fname_clm <- paste(clm_cru_odir,"/afr_",vname,"_meanclim_",min(years),"-",max(years),"_",sprintf("%1$02d",m),".tif",sep="")
+    cmth_fname_var <- paste(clm_cru_odir,"/afr_",vname,"_stdclim_",min(years),"-",max(years),"_",sprintf("%1$02d",m),".tif",sep="")
+    cmth_fname_cv <- paste(clm_cru_odir,"/afr_",vname,"_cvclim_",min(years),"-",max(years),"_",sprintf("%1$02d",m),".tif",sep="")
+    
+    #load all years rasters
+    for (yr in years) {
+      #yr <- years[1]
+      rs_tstk <- stack(paste(cru_ovdir,"/afr_",vname,"_",yr,".nc",sep=""))
+      if (yr == years[1]) {rs_stk <- rs_tstk[[m]]} else {rs_stk <- c(rs_stk, rs_tstk[[m]])}
+    }
+    rs_stk <- stack(rs_stk)
+    
+    #mean
+    if (!file.exists(cmth_fname_clm)) {
+      rs_mmean <- calc(rs_stk, function(x) {mean(x,na.rm=T)})
+      rs_mmean <- writeRaster(rs_mmean,cmth_fname_clm,format="GTiff")
+    } else {
+      rs_mmean <- raster(cmth_fname_clm)
+    }
+    
+    #s.d.
+    if (!file.exists(cmth_fname_var)) {
+      rs_mvar <- calc(rs_stk, function(x) {sd(x,na.rm=T)})
+      rs_mvar <- writeRaster(rs_mvar,cmth_fname_var,format="GTiff")
+    } else {
+      rs_mvar <- raster(cmth_fname_var)
+    }
+    
+    #c.v.
+    if (!file.exists(cmth_fname_cv)) {
+      if (vname == "pre") {
+        rs_mcv <- rs_mvar / (rs_mmean+0.1) * 100
+      } else {
+        rs_mcv <- rs_mvar / (rs_mmean) * 100
+      }
+      rs_mcv <- writeRaster(rs_cv,cmth_fname_cv,format="GTiff")
+    }
+  }
+}
+
+
+################################################################################
+################################################################################
+#aggregate worldclim to 0.5 degree, then remapcon2, then cut to Africa
+
+wcl_idir <- "~/Leeds-work/datasets/meteorology/worldclim_global_5min"
+wcl_odir <- paste(met_dir,"/worldclim",sep="")
+if (!file.exists(wcl_odir)) {dir.create(wcl_odir)}
+
+vname <- "prec"
+
+for (m in 1:12) {
+  #m <- 1
+  #load raster
+  rs <- raster(paste(wcl_idir,"/",vname,"_",m,sep=""))
+  
+  #aggregate to 0.5 degree
+  rs <- aggregate(rs, fact=6, fun=mean, expand=T, na.rm=T)
+  
+  #write .nc
+  
+  #remapcon2
+  
+  #cut to Africa
+  
+}
+
+
+
+
