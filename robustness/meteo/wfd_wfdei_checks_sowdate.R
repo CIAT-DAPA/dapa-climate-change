4331a375b08518744f16e9fd1d56e7e88641d50d
dawnpatrolmustaine <dawnpatrolmustaine@gmail.com>
2014-04-03 21:20:39 +0000

    update robustness scripts

--- robustness/meteo/wfd_wfdei_checks_sowdate.R
+++ robustness/meteo/wfd_wfdei_checks_sowdate.R
@@ -3,7 +3,8 @@
 #Apr 2014
 
 #############################################################################################
-####### See wfd_wfdei_checks.R 
+####### See wfd_wfdei_checks.R. I realised the annual precip totals were ok, so calculated
+####### totals of precip for the Sacks et al. (2010) defined growing season.
 #############################################################################################
 
 #packages
@@ -21,23 +22,12 @@ load(paste(mdata_dir,"/initial_conditions_major.RData",sep=""))
 load(paste(mdata_dir,"/yield_major.RData",sep=""))
 
 #define dataset, period, variable
-dataset <- "WFDEI" #WFD, WFDEI
-years <- 1982:2005 #1982:2001 for WFD, 1982:2005 for WFDEI
-
-#i/o directories for cru
-cru_idir <- "~/Leeds-work/datasets/meteorology/cru-ts-v3-21"
-cru_odir <- paste(met_dir,"/cru-ts-v3-21",sep="")
-if (!file.exists(cru_odir)) {dir.create(cru_odir)}
-
-#i/o directories for worldclim
-wcl_idir <- "~/Leeds-work/datasets/meteorology/worldclim_global_5min"
-wcl_odir <- paste(met_dir,"/worldclim",sep="")
-if (!file.exists(wcl_odir)) {dir.create(wcl_odir)}
+dataset <- "WFD" #WFD, WFDEI
+years <- 1982:2001 #1982:2001 for WFD, 1982:2005 for WFDEI
 
 #/o directory for figs
 fig_dir <- paste(wd,"/text/wfd_wfdei_checks/",tolower(dataset),sep="")
-if (!file.exists(fig_dir)) {dir.create(fig_dir,recursive=T)}
 
-#determine extent to cut the resampled netcdfs (of CRU and WorldClim)
-yrs <- raster(paste(yi_dir,"/descriptive_stats/mean_ModelYld500.tif",sep=""))
-bbox <- extent(yrs)
+
+
+
