c1a9a12923a529fbd6bb2771eb407893d62baf21
dawnpatrolmustaine@gmail.com <dawnpatrolmustaine@gmail.com@38c049bd-fe20-d486-6346-6a8e9033181b>
2014-04-25 13:25:58 +0000

    update optimise.R in robustness study

--- trunk/robustness/glam-utils/optimise.R
+++ trunk/robustness/glam-utils/optimise.R
@@ -72,6 +72,7 @@ opt_data$PARAMS$glam_param.maize$TLIMJUV$Value <- 280
 ### note:
 #simulate year before starting one because if sowing date is late then harvest is in this year
 #last year cannot be last year of time series since model runs could fail due to late sowing
+#see function GLAM_calibrate() in calibrate.R for details on how this is done.
 
 #calibrate ygp
 GLAM_optimise <- function(opt_data) {
@@ -132,14 +133,15 @@ GLAM_optimise <- function(opt_data) {
   opt_data$PARAMS$glam_param.mod_mgt$IASCII <- 1 #output only to season file
   
   #file of output
-  cal_outfile <- paste(opt_dir,"/opt-",opt_data$PARAM,".txt",sep="")
+  cal_outfile <- paste(opt_dir,"/opt-",opt_data$PARAM,".txt",sep="") #summary
+  raw_outfile <- paste(opt_dir,"/cal-",opt_data$PARAM,"_raw.txt",sep="") #raw
   
   #do only if calibration file does not exist
   if (!file.exists(cal_outfile)) {
     #loop through sequence of values
     for (i in 1:length(vals)) {
       #i <- 1
-      cat("performing run ",opt_data$RUN_TYPE," ",i," value = ",vals[i]," (",param,")",sep="","\n")
+      cat("\nperforming run ",opt_data$RUN_TYPE," ",i," value = ",vals[i]," (",param,")",sep="","\n")
       
       #assign values to parameter set
       opt_data$PARAMS[[sect]][[param]][,"Value"] <- vals[i]
@@ -147,55 +149,64 @@ GLAM_optimise <- function(opt_data) {
       #calibrate model
       cal_data <- opt_data
       cal_data$BASE_DIR <- opt_dir
-      cal_data$SIM_NAME <- "calibration"
-      cal_data$NSTEPS <- 50
+      cal_data$SIM_NAME <- paste("calibration_",cal_data$PARAM,"_run-",i,sep="")
+      cal_data$NSTEPS <- 46
       ygp_calib <- GLAM_calibrate(cal_data)
       
       #yearly output
       yr_out <- ygp_calib$RAW_DATA
       
+      #select optimal ygp of each grid cell
+      ygp_all <- data.frame()
+      for (loc in unique(yr_out$LOC)) {
+        ygp_opt <- min(ygp_calib$CALIBRATION$RMSE[which(ygp_calib$CALIBRATION$LOC == loc)])
+        ygp_opt <- ygp_calib$CALIBRATION$VALUE[which(ygp_calib$CALIBRATION$LOC == loc & ygp_calib$CALIBRATION$RMSE == ygp_opt)]
+        if (length(ygp_opt) > 1) {ygp_opt <- max(ygp_opt)}
+        ygp_loc <- yr_out[which(yr_out$LOC == loc & yr_out$VALUE == ygp_opt),]
+        ygp_all <- rbind(ygp_all, ygp_loc)
+      }
       
       #choose optimisation method (RMSE, CH07, CH10)
       if (opt_meth == "RMSE") {
-        rmse <- sqrt(sum((yr_out$OBS_ADJ-yr_out$PRED_ADJ)^2,na.rm=T) / (length(which(!is.na(yr_out$OBS_ADJ)))))
+        rmse <- sqrt(sum((ygp_all$OBS_ADJ-ygp_all$PRED_ADJ)^2,na.rm=T) / (length(which(!is.na(ygp_all$OBS_ADJ)))))
       } else if (opt_meth == "CH07") {
-        rmse <- (mean(odf$OBS,na.rm=T)-mean(odf$PRED,na.rm=T))^2 + (sd(odf$OBS,na.rm=T)-sd(odf$PRED,na.rm=T))^2
+        #do for individual cells and add them up
+        rmse <- 0
+        for (loc in unique(ygp_all$LOC)) {
+          rmse_loc <- ygp_all[which(ygp_all$LOC == loc),]
+          rmse_loc <- (mean(rmse_loc$OBS_ADJ,na.rm=T)-mean(rmse_loc$PRED_ADJ,na.rm=T))^2 + (sd(rmse_loc$OBS,na.rm=T)-sd(rmse_loc$PRED,na.rm=T))^2
+          rmse <- rmse + rmse_loc
+        }
       } else if (opt_meth == "CH10") {
-        rmse <- (mean(odf$OBS,na.rm=T)-mean(odf$PRED,na.rm=T))^2
+        rmse <- 0
+        for (loc in unique(ygp_all$LOC)) {
+          rmse_loc <- ygp_all[which(ygp_all$LOC == loc),]
+          rmse_loc <- (mean(rmse_loc$OBS_ADJ,na.rm=T)-mean(rmse_loc$PRED_ADJ,na.rm=T))^2
+          rmse <- rmse + rmse_loc
+        }
+        rmse <- sqrt(rmse/length(unique(ygp_all$LOC)))
       }
-      
-      out_row <- data.frame(VALUE=vals[i],RMSE=rmse,YOBS=mean(odf$OBS,na.rm=T), YPRED=mean(odf$PRED,na.rm=T))
+      out_row <- data.frame(VALUE=vals[i],RMSE=rmse,YOBS=mean(ygp_all$OBS,na.rm=T), YPRED=mean(ygp_all$PRED,na.rm=T))
+      names(ygp_all)[3] <- "YGP"
+      ygp_all$VALUE <- vals[i]
       
       if (i == 1) {
         out_all <- out_row
+        out_raw <- ygp_all
       } else {
         out_all <- rbind(out_all,out_row)
+        out_raw <- rbind(out_raw,ygp_all)
       }
     }
     #write outputs for this grid cell
     write.table(out_all,sep="\t",quote=F,file=cal_outfile,row.names=F)
+    write.table(out_raw,sep="\t",quote=F,file=raw_outfile,row.names=F)
   } else {
     out_all <- read.table(cal_outfile,sep="\t",header=T)
-  }
-  
-  #append location data
-  out_all <- cbind(LOC=loc,out_all)
-  raw_all <- cbind(LOC=loc,raw_all)
-  if (loc == opt_data$LOC[1]) {
-    cal_all <- out_all
-    raw_cal <- raw_all
-  } else {
-    cal_all <- rbind(cal_all, out_all)
-    raw_cal <- rbind(raw_cal, raw_all)
-  }
-  
-  #remove junk from scratch
-  if (opt_data$USE_SCRATCH) {
-    system(paste("cp -rf ",cal_dir," ",paste(nfs_dir,"/.",sep=""),sep=""))
-    system(paste("rm -rf ",cal_dir,sep=""))
+    out_raw <- read.table(raw_outfile,sep="\t",header=T)
   }
   
   #return object
-  r_list <- list(CALIBRATION=cal_all, RAW_DATA=raw_cal)
-  return(cal_all)
+  r_list <- list(OPTIMISATION=out_all, RAW_DATA=out_raw)
+  return(r_list)
 }
