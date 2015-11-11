849a3aceeebefd00b230eaabc1b4ecfd82897cab
jheduart <jaime.tm8@gmail.com>
2015-11-11 14:08:06 -0500

    new scripts

--- IPCC-CMIP5/07-ZipGrids_process.py
+++ IPCC-CMIP5/07-ZipGrids_process.py
@@ -1,78 +1,138 @@
-# ---------------------------------------------------------------------------------
-# Author: Jaime Tarapues
-# Date: 11/2013
-# Purpose: Grid to zip CMIP5
-# ----------------------------------------------------------------------------------
+# ---------------------------------------------------------
+# Author: Carlos Navarro
+# Purpouse: Copy rasters to another location
+# ---------------------------------------------------------
+# python 07-ZipGrids_process.py T:\gcm\cmip5\downscaled S:\portals\ccafs_climate\download_data\files\data\ipcc_5ar_ciat_downscaled rcp26 10min
 
 
-# python G:\_scripts\dapa-climate-change\IPCC-CMIP5\07-ZipGrids_process.py T:\data\gcm\cmip5\downscaled S:\data\portals\ccafs_climate\download_data\files\data\ipcc_5ar_ciat_downscaled rcp26 10min
 
-import arcgisscripting, os, sys, string, shutil
+import arcgisscripting, os, sys, glob
 gp = arcgisscripting.create(9.3)
 
-rootdir = sys.argv[1]
-outputdir = sys.argv[2]
+if len(sys.argv) < 4:
+	os.system('cls')
+	print "\n Too few args"
+	print " Syntaxis python CopyRasters.py <dirbase> <dirout> <wildcard> <switch>"
+	print "   - ex: python CopyRasters.py D:\Workspace D:\Workspace ALL NO"
+	sys.exit(1)
+
+# Arguments
+dirbase = sys.argv[1]
+dirout = sys.argv[2]
 rcp = sys.argv[3]
-resol = sys.argv[4]
+res = sys.argv[4]
 
+print "\n~~~~ COPY RASTERS ~~~~\n"
 
-# Clean screen
+# Clear screen
 os.system('cls')
 
-#Check out Spatial Analyst extension license
-gp.CheckOutExtension("Spatial")
-gp.toolbox="management"
+# Set workspace
+# gp.workspace = dirbase 
 
-tmp = "G:\jetarapues\cmip5_process"
+# Create output folder
+if not os.path.exists(dirout):
+    os.system('mkdir ' + dirout)
 
-if rcp == "rcp26": 
-	rcpmod = "rcp2_6"
-elif rcp == "rcp45": 
-	rcpmod = "rcp4_5"
-elif rcp == "rcp60": 
-	rcpmod = "rcp6_0"
-else:
-	rcpmod = "rcp8_5"
+# Get a list of grids in the workspace
+# print "\t ..listing grids into " + dirbase
+# if wildcard == "ALL":
+	# rasters = sorted(gp.ListRasters("*", "GRID"))
+# else:	
+	# rasters = sorted(gp.ListRasters(wildcard + "*", "GRID"))
+	
+# Lopping around the grids
 
 periodlist = ["2020_2049", "2040_2069", "2060_2089", "2070_2099"]
-vartype = ["bio", "cons", "prec", "tmax", "tmean", "tmin"]	
-
-modellist = sorted(os.listdir(rootdir + "\\" + rcp + "\\global_" +resol))
+modellist = sorted(os.listdir(dirbase + "\\" + rcp + "\\global_" + res))
+variablelist = ["bio","cons_mths","prec"] #["bio","cons_mths","prec","tmin","tmax","tmean" ]
+# modellist = ["ipsl_cm5a_lr"]
+# for rcp in rcpList:		
 for period in periodlist:
 	for model in modellist:
+		
+		if period == "2020_2049":
+			tsmod= "2030s"
+		elif period=="2040_2069":
+			tsmod= "2050s"
+		elif period== "2060_2089":
+			tsmod= "2070s"
+		else:
+			tsmod= "2080s"
+		
+		if rcp== "rcp26":
+			rcpmod ="rcp2_6"
+		elif rcp== "rcp45":
+			rcpmod ="rcp4_5"
+		elif rcp== "rcp60":
+			rcpmod ="rcp6_0"
+		else:
+			rcpmod= "rcp8_5"
+	
+		outfolder="S:\\portals\\ccafs_climate\\download_data\\files\\data\\ipcc_5ar_ciat_downscaled"+ "\\" +rcpmod+ "\\" +tsmod+ "\\" +model+ "\\" +res
+		descfile = outfolder+"_grd2zip_done2.txt"
+		if not os.path.exists(descfile): 
+			for var in variablelist:
+				
+				if var == "bio":
+					num = 19
+				elif var == "cons_mths":
+					num = 1				
+				else:
+					num = 12
+				# for month in [1,12]:
+				
+				
+				# descfile = dirout+ "\\" +rcpmod+ "\\" +tsmod+ "\\" +model+ "\\" +res+"_grd2zip_done2.txt"
+				
+				
+				
+				outmoddir = dirout+ "\\" +rcpmod+ "\\" +tsmod+ "\\" +model+ "\\" +res
+				if not os.path.exists(outmoddir+ "\\" +var):
+					os.system("mkdir "+outmoddir+ "\\" +var)			
+				print "......",rcp,period, model,"\n"
+				for month in range(1,num+1):
+					if var == "cons_mths":
+						variable = var
+					else:
+						variable = var + "_" + str(month)
+						
+					workspace = dirbase + "\\" + rcp + "\\global_" + res + "\\" + model + "\\r1i1p1\\" + period	
 
-		if period == "2020_2049": 
-			tsmod = "2030s"
-		elif period == "2040_2069": 
-			tsmod = "2050s"
-		elif period == "2060_2089":
-			tsmod = "2070s"
-		else:
-			tsmod = "2080s"	
-			
-		checkFile = outputdir+ "\\" +rcpmod+ "\\" +tsmod+ "\\" +model+ "\\" +resol + "_grd2zip_done.txt"
-		if not os.path.exists(checkFile):
-			gp.workspace = rootdir + "\\" + rcp + "\\global_" +resol + "\\" + model + "\\r1i1p1\\" + period
-			outmoddir = outputdir+ "\\" +rcpmod+ "\\" +tsmod+ "\\" +model+ "\\" +resol
-			print "==== Processing",rcp, resol, model, period+" ("+tsmod+")\n"
-			rasters = sorted(gp.ListRasters("*", "GRID"))
-			for raster in rasters:
-				outgrid = tmp+ "\\" +rcpmod+ "\\" +tsmod+ "\\" +model+ "\\" +resol+ "\\" +raster.split("_")[0]
-				inzip = outmoddir+ "\\" +model+"_"+rcpmod+"_"+tsmod+"_"+raster.split("_")[0]+"_"+resol+"_r1i1p1_no_tile_grd.zip"
-				if not os.path.exists(inzip):
-					if not os.path.exists(outgrid):
-						os.system('mkdir ' + outgrid)
-					if not os.path.exists(outgrid+ "\\" +raster):	
-						gp.CopyRaster_management(raster,outgrid+ "\\" +raster)	
-					print "Copied ", outgrid+ "\\" +raster	
-			for raster in vartype:
-				outgrid = tmp+ "\\" +rcpmod+ "\\" +tsmod+ "\\" +model+ "\\" +resol+ "\\" +raster
-				inzip = outmoddir+ "\\" +model+"_"+rcpmod+"_"+tsmod+"_"+raster+"_"+resol+"_r1i1p1_no_tile_grd.zip"
-				if not os.path.exists(inzip):			
-					os.system('7za a -mmt=4 ' + inzip + " " + outgrid)
-					shutil.rmtree(outgrid)
-				print "\n " + model,rcpmod,tsmod,raster,resol + " processed\n"					
-			checkFile = open(checkFile, "w")
-			checkFile.close()				
-		else:
-			print "\n " + rcp, resol, model, period+" ("+tsmod+")" + " processed\n"			
+					raster= workspace+"\\"+variable
+
+					if not gp.Exists(outmoddir+ "\\" +var + "\\" + variable):
+						gp.copy_management(raster, outmoddir+ "\\" +var+ "\\" +variable)
+						print "\t", variable, "copied"
+					else:
+						print "\t", variable, "already exist!"
+				
+				if var == "cons_mths":
+					vartype=var.split("_")[0]	
+					
+					if os.path.exists(outfolder+ "\\" +model+ "_" +rcpmod+ "_" +tsmod+ "_" +vartype+ "_" +res+ "_r1i1p1_no_tile_grd.zip"): 
+						os.system("del "+outfolder+ "\\" +model+ "_" +rcpmod+ "_" +tsmod+ "_" +vartype+ "_" +res+ "_r1i1p1_no_tile_grd.zip")
+					os.system(" 7za a -mmt=4 "+ outfolder+ "\\" +model+ "_" +rcpmod+ "_" +tsmod+ "_" +vartype+ "_" +res+ "_r1i1p1_no_tile_grd.zip "+ outmoddir+ "\\" +var)
+					os.system("rmdir /s /q "+outmoddir+ "\\" +var)
+				else:
+					if os.path.exists(outfolder+ "\\" +model+ "_" +rcpmod+ "_" +tsmod+ "_" +var+ "_" +res+ "_r1i1p1_no_tile_grd.zip"): 
+						os.system("del "+outfolder+ "\\" +model+ "_" +rcpmod+ "_" +tsmod+ "_" +var+ "_" +res+ "_r1i1p1_no_tile_grd.zip")
+					os.system(" 7za a -mmt=4 "+ outfolder+ "\\" +model+ "_" +rcpmod+ "_" +tsmod+ "_" +var+ "_" +res+ "_r1i1p1_no_tile_grd.zip "+ outmoddir+ "\\" +var)
+					os.system("rmdir /s /q "+outmoddir+ "\\" +var)				
+			outFile = open(descfile, "w")
+
+				
+		
+# for raster in rasters:
+
+	# if not gp.Exists(dirout + "\\" + raster):
+	
+		# Copy function
+		# gp.copy_management(raster, dirout + "\\" + raster)
+		# print "\t", raster, "copied"
+	
+		# Remove asciis
+		# if switch == "YES":
+			# gp.delete_management(raster)
+		
+print "\n \t Process done!!"
