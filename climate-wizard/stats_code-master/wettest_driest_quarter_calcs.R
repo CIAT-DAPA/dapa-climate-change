02f4fb9b40314ddeae0de1938d1c1bc3fbb95b4e
Carlos Eduardo Navarro Racines <c.e.navarro@cgiar.org>
2016-12-09 16:57:44 -0500

    wet dry calcs climate wizard

--- climate-wizard/stats_code-master/wettest_driest_quarter_calcs.R
+++ climate-wizard/stats_code-master/wettest_driest_quarter_calcs.R
@@ -8,10 +8,11 @@ require(raster)
 require(ncdf4)
 
 ## Arguments
-iDir <- paste0("F:/ClimateWizard/data/AR5_Global_Daily_25k/out_stats/", gcm)
 rcp <- "historical" # "rcp45", "rcp85"
 iY <- 1950 #2006
 eY <- 2005 #2099
+iRY <- 1950 
+eRY <- 1999
 thr = -1.5 #Threshold severe dryness
 
 ## GCM List
@@ -21,16 +22,26 @@ gcmList <- c("ACCESS1-0", "bcc-csm1-1", "BNU-ESM", "CanESM2", "CCSM4", "CESM1-BG
 # added as fgobal institution attribute to output files
 txtinst = "Santa Clara U.,Climate Central,The Nature Conservancy,International Center for Tropical Agriculture"
 
-## Loop around GCMs
+## Loop around GCMs and run functions
 for (gcm in gcmList){
-  # gcm <- "GFDL-CM3"
+  
+  gcm <- "GFDL-CM3"
+  iDir <- paste0("/mnt/data_climatewizard/AR5_Global_Daily_25k/out_stats/", gcm)
+#   iDir <- paste0("F:/ClimateWizard/data/AR5_Global_Daily_25k/out_stats/", gcm)
+  tmpDir <- paste0("/mnt/data_climatewizard/AR5_Global_Daily_25k/", gcm, "/junk")
+# tmpDir <- paste0("F:/ClimateWizard/data/AR5_Global_Daily_25k/", gcm, "/junk")
   wetSeasonIndices(iDir, rcp, iY, eY)
   drySeasonIndices(iDir, rcp, iY, eY)
-  spi_1month(iDir, rcp, iY, eY, thr)
+  spi_1month(iDir, tmpDir, rcp, iY, eY, thr)
+  
 }
 
+# Temporal dir for raster library
+rstmpDir <- "/mnt/data_climatewizard/AR5_Global_Daily_25k/temp"
+if (!file.exists(rstmpDir))) {dir.create(rstmpDir, recursive = TRUE)}
+rasterOptions(tmpdir= rstmpDir)
 
-wetSeasonIndices <- function(iDir="", rcp="", iY="", eY=""){
+wetSeasonIndices <- function(iDir="", tmpDir="", rcp="", iY="", eY=""){
 
   # iDir <- dirname(fn)
   
@@ -57,7 +68,7 @@ wetSeasonIndices <- function(iDir="", rcp="", iY="", eY=""){
       # Get wettest quarter for each year and then concatenate all years
       for (y in 1:(eY-iY+1)){
         
-        cat("Wet calcs: year", y, "/", eY-iY, "\n")
+        cat("Wet calcs: year", paste0(y, "/", eY-iY), "\n")
         qSum <- stack()
         
         for (i in 1:10){
@@ -91,9 +102,10 @@ wetSeasonIndices <- function(iDir="", rcp="", iY="", eY=""){
       }
       
       # Write PWET and set years to time axis
-      writeRaster(pwet, paste0(iDir, "/pwet_tmp.nc", overwrite=T))
-      system(paste0("cdo settaxis,", iY, "-01-00,12:00:00,1year ", iDir, "/pwet_tmp.nc ", oPwet))
-      unlink(paste(iDir, "pwet_tmp.nc"))
+      cat(" Writting PWET", "\n")
+      writeRaster(pwet, paste0(tmpDir, "/pwet_tmp.nc", overwrite=T))
+      system(paste0("cdo settaxis,", iY, "-01-00,12:00:00,1year ", tmpDir, "/pwet_tmp.nc ", oPwet))
+      unlink(paste(tmpDir, "pwet_tmp.nc"))
       
       # Edit PWET file
       nc_edit <- nc_open(oPwet, write = T)
@@ -102,9 +114,10 @@ wetSeasonIndices <- function(iDir="", rcp="", iY="", eY=""){
       nc_close(nc_edit)
       
       # Write R02WET and set years to time axis
-      writeRaster(r02wet, paste0(iDir, "/r02wet_tmp.nc", overwrite=T))
-      system(paste0("cdo settaxis,", iY, "-01-00,12:00:00,1year ", iDir, "/r02wet_tmp.nc ", oR02wet))
-      unlink(paste(iDir, "r02wet_tmp.nc"))
+      cat(" Writting R02WET", "\n")
+      writeRaster(r02wet, paste0(tmpDir, "/r02wet_tmp.nc", overwrite=T))
+      system(paste0("cdo settaxis,", iY, "-01-00,12:00:00,1year ", tmpDir, "/r02wet_tmp.nc ", oR02wet))
+      unlink(paste(tmpDir, "r02wet_tmp.nc"))
       
       # Edit R02WET file
       nc_edit <- nc_open(oR02wet, write = T)
@@ -112,7 +125,8 @@ wetSeasonIndices <- function(iDir="", rcp="", iY="", eY=""){
       ncatt_put(nc_edit, 0, "Institution", txtinst)
       nc_close(nc_edit)
       
-      # Write SDII 
+      # Write SDIIWET 
+      cat(" Writting SDIIWET", "\n")
       system(paste0("cdo mondiv ", oPwet," ", oR02wet, " ", oSdiiwet))
       
       # Edit TASWET file
@@ -122,15 +136,22 @@ wetSeasonIndices <- function(iDir="", rcp="", iY="", eY=""){
       nc_close(nc_edit)
       
       # Write TASWET and set years to time axis
-      writeRaster(taswet, paste0(iDir, "/taswet_tmp.nc", overwrite=T))
-      system(paste0("cdo settaxis,", iY, "-01-00,12:00:00,1year ", iDir, "/taswet_tmp.nc ", oTaswet))
-      unlink(paste(iDir, "taswet_tmp.nc"))
+      cat(" Writting TASWET", "\n")
+      writeRaster(taswet, paste0(tmpDir, "/taswet_tmp.nc", overwrite=T))
+      system(paste0("cdo settaxis,", iY, "-01-00,12:00:00,1year ", tmpDir, "/taswet_tmp.nc ", oTaswet))
+      unlink(paste(tmpDir, "taswet_tmp.nc"))
       
       # Edit TASWET file
       nc_edit <- nc_open(oTaswet, write = T)
       ncvar_rename(nc_edit, "variable", "taswet")
       ncatt_put(nc_edit, 0, "Institution", txtinst)
       nc_close(nc_edit)
+      
+      cat(" Done!\n")
+      
+    } else {
+      
+      cat("\n... nothing to do, Wet files exists!\n")
       
     }
     
@@ -139,7 +160,7 @@ wetSeasonIndices <- function(iDir="", rcp="", iY="", eY=""){
 }
 
 
-drySeasonIndices <- function(iDir="", rcp="", iY="", eY=""){
+drySeasonIndices <- function(iDir="", tmpDir="", rcp="", iY="", eY=""){
   
   # iDir <- dirname(fn)
   
@@ -166,7 +187,7 @@ drySeasonIndices <- function(iDir="", rcp="", iY="", eY=""){
       # Get drytest quarter for each year and then concatenate all years
       for (y in 1:(eY-iY+1)){
         
-        cat("Dry calcs: year", y, "/", eY-iY, "\n")
+        cat("Dry calcs: year", paste0(y, "/", eY-iY), "\n")
         qSum <- stack()
         
         for (i in 1:10){
@@ -200,9 +221,10 @@ drySeasonIndices <- function(iDir="", rcp="", iY="", eY=""){
       }
       
       # Write PDRY and set years to time axis
-      writeRaster(pdry, paste0(iDir, "/pdry_tmp.nc", overwrite=T))
-      system(paste0("cdo settaxis,", iY, "-01-00,12:00:00,1year ", iDir, "/pdry_tmp.nc ", oPdry))
-      unlink(paste(iDir, "pdry_tmp.nc"))
+      cat(" Writting PDRY", "\n")
+      writeRaster(pdry, paste0(tmpDir, "/pdry_tmp.nc", overwrite=T))
+      system(paste0("cdo settaxis,", iY, "-01-00,12:00:00,1year ", tmpDir, "/pdry_tmp.nc ", oPdry))
+      unlink(paste(tmpDir, "pdry_tmp.nc"))
       
       # Edit PDRY file
       nc_edit <- nc_open(oPdry, write = T)
@@ -211,9 +233,10 @@ drySeasonIndices <- function(iDir="", rcp="", iY="", eY=""){
       nc_close(nc_edit)
       
       # Write R02DRY and set years to time axis
-      writeRaster(r02dry, paste0(iDir, "/r02dry_tmp.nc", overwrite=T))
-      system(paste0("cdo settaxis,", iY, "-01-00,12:00:00,1year ", iDir, "/r02dry_tmp.nc ", oR02dry))
-      unlink(paste(iDir, "r02dry_tmp.nc"))
+      cat(" Writting R02DRY", "\n")
+      writeRaster(r02dry, paste0(tmpDir, "/r02dry_tmp.nc", overwrite=T))
+      system(paste0("cdo settaxis,", iY, "-01-00,12:00:00,1year ", tmpDir, "/r02dry_tmp.nc ", oR02dry))
+      unlink(paste(tmpDir, "r02dry_tmp.nc"))
       
       # Edit R02DRY file
       nc_edit <- nc_open(oR02dry, write = T)
@@ -221,7 +244,8 @@ drySeasonIndices <- function(iDir="", rcp="", iY="", eY=""){
       ncatt_put(nc_edit, 0, "Institution", txtinst)
       nc_close(nc_edit)
       
-      # Write SDII 
+      # Write SDIIDRY 
+      cat(" Writting SDIIDRY", "\n")
       system(paste0("cdo mondiv ", oPdry," ", oR02dry, " ", oSdiidry))
       
       # Edit TASDRY file
@@ -231,15 +255,22 @@ drySeasonIndices <- function(iDir="", rcp="", iY="", eY=""){
       nc_close(nc_edit)
       
       # Write TASDRY and set years to time axis
-      writeRaster(tasdry, paste0(iDir, "/tasdry_tmp.nc", overwrite=T))
-      system(paste0("cdo settaxis,", iY, "-01-00,12:00:00,1year ", iDir, "/tasdry_tmp.nc ", oTasdry))
-      unlink(paste(iDir, "tasdry_tmp.nc"))
+      cat(" Writting TASDRY", "\n")
+      writeRaster(tasdry, paste0(tmpDir, "/tasdry_tmp.nc", overwrite=T))
+      system(paste0("cdo settaxis,", iY, "-01-00,12:00:00,1year ", tmpDir, "/tasdry_tmp.nc ", oTasdry))
+      unlink(paste(tmpDir, "tasdry_tmp.nc"))
       
       # Edit TASDRY file
       nc_edit <- nc_open(oTasdry, write = T)
       ncvar_rename(nc_edit, "variable", "tasdry")
       ncatt_put(nc_edit, 0, "Institution", txtinst)
       nc_close(nc_edit)
+      
+      cat(" Done!\n")
+      
+    } else {
+      
+      cat("\n... nothing to do, Dry files exists!\n")
       
     }
     
@@ -248,7 +279,7 @@ drySeasonIndices <- function(iDir="", rcp="", iY="", eY=""){
 }
 
 
-spi_1month <- function(fn="", rcp="", iY="", eY="", thr=""){
+spi_1month <- function(fn="", tmpDir="", rcp="", iY="", eY="", iRY="", eRY="", thr=""){
   
   ## Documentation about spi: 
   ## http://www.wamis.org/agm/pubs/SPI/WMO_1090_EN.pdf 
@@ -258,43 +289,82 @@ spi_1month <- function(fn="", rcp="", iY="", eY="", thr=""){
   # -1.5 to -1.99 Severe dryness
   # < -2.0 Extreme dryness
   
-  # iDir <- dirname(fn)
-  # iDir <- paste0("D:/CIAT/Projects/wocat/AR5_Global_Daily_25k/out_stats/", gcm)
-  # iDir <- paste0("//ccafsserver/ClimateWizard/data/AR5_Global_Daily_25k/out_stats/", gcm)
-  
-  oDroi <- paste0(iDir, "/DROI_BCSD_", rcp, "_", gcm, "_", iY, "_", eY, ".nc")
-  oDrof <- paste0(iDir, "/DROF_BCSD_", rcp, "_", gcm, "_", iY, "_", eY, ".nc")
-  
-  nYears <- eY - iY + 1 
-  pr_stk <- stack(paste0(iDir, "/PTOT_BCSD_", rcp, "_", gcm, "_", iY, "_", eY, ".monthly.nc"))
+  oDroi <- paste0(iDir, "/DROI_BCSD_", rcp, "_", gcm, "_", iY, "-", eY, ".nc")
+  oDrof <- paste0(iDir, "/DROF_BCSD_", rcp, "_", gcm, "_", iY, "-", eY, ".nc")
   
   
+  if (!file.exists(oDroi)){
+      
+    pr_mth <- paste0(iDir, "/PTOT_BCSD_", rcp, "_", gcm, "_", iY, "-", eY, ".monthly.nc")
+    pr_clim <- paste0(tmpDir, "/PTOT_BCSD_REFCLIM_",  rcp, "_", gcm, "_", iRY, "-", eRY, ".nc")
   
-  pr_stk_ref <- stack(paste0(iDir, "/PTOT_BCSD_30yravg_", gcm, "_", iY, "_", eY, ".nc"))
+    # Calc the reference climatology
+    system(paste0("cdo ymonmean -selyear,", paste(1950:1999, collapse=","), " ", pr_mth, " ", pr_clim))
+    
+    ## Calc Anomaly, avg and std
+    pr_anom <- paste0(tmpDir, "/PTOT_BCSD_CHG_", rcp, "_", gcm, "_", iY, "-", eY, ".monthly.nc")
+    pr_anom_avg <- paste0(tmpDir, "/PTOT_BCSD_CHGAVG_", rcp, "_", gcm, "_", iY, "-", eY, ".monthly.nc")
+    pr_anom_std <- paste0(tmpDir, "/PTOT_BCSD_CHGSTD_", rcp, "_", gcm, "_", iY, "-", eY, ".monthly.nc")
+    system(paste0("cdo ymonsub ", pr_mth, " ", pr_clim, " ", pr_anom))
+    system(paste0("cdo ymonavg ", pr_anom, " ", pr_anom_avg))
+    system(paste0("cdo ymonstd ", pr_anom, " ", pr_anom_std))
+    
+    # SPI Calculation
+    # Normalized climatology and divide over std clim
+    pr_norm <- paste0(tmpDir, "/PTOT_BCSD_CHGNOR_", rcp, "_", gcm, "_", iY, "-", eY, ".monthly.nc")
+    pr_spi <- paste0(tmpDir, "/PTOT_BCSD_SPI_", rcp, "_", gcm, "_", iY, "-", eY, ".monthly.nc")
+    system(paste0("cdo ymonsub ", pr_anom, " ", pr_anom_avg, " ", pr_norm))
+    system(paste0("cdo ymondiv ", pr_norm, " ", pr_anom_std, " ", pr_spi))
+    
+    ## Write SPI in root folder 
+    file.rename(pr_spi, oDroi)
+    unlink(paste0(tmpDir, "/PTOT*.nc"))
+    
+    # Edit DROI file
+    nc_edit <- nc_open(oDroi, write = T)
+    ncvar_rename(nc_edit, "PTOT", "droi")
+    ncatt_put(nc_edit, 0, "Institution", txtinst)
+    nc_close(nc_edit)
+    
+  } else {
+    
+    cat("\n... nothing to do DROI file exists!\n")
+    
+  }
   
-  ## Anomaly month i
+  if (!file.exists(oDrof)){
+    
+    # Calculate frequence of SPI
+    droi_stk <- stack(oDroi)
+    for (i in 1:nlayers(droi_stk)){
+      
+      cat(paste0("Freq calc ", i, "/", nlayers(droi_stk), "\n"))
+      spi_f <- droi_stk[[i]]
+      spi_f[which(spi_f[]>= thr)] <- 0
+      spi_f[which(spi_f[]< thr)] <- (1/12)
+      droi_stk[[i]] <- spi_f
+      
+    }
+    
+    # Write SPI (drof) and set years to time axis
+    writeRaster(droi_stk, paste0(tmpDir, "/drof_tmp_monthly.nc"))
+    system(paste0("cdo settaxis,", iY, "-01-00,00:00:00,1mon ", tmpDir, "/drof_tmp_monthly.nc ", tmpDir, "/drof_tmp_monthly_set_time.nc"))
+    system(paste0("cdo yearsum ", tmpDir, "/drof_tmp_monthly_set_time.nc ", tmpDir, "/drof_yearly.nc "))
+    file.rename(paste0(tmpDir, "/drof_yearly.nc"), oDrof)
+    unlink(paste0(tmpDir, "/drof*.nc"))
+    
+    # Edit DROF file
+    nc_edit <- nc_open(oDrof, write = T)
+    ncvar_rename(nc_edit, "variable", "drof")
+    ncatt_put(nc_edit, 0, "Institution", txtinst)
+    nc_close(nc_edit)
+    
+  } else {
+    
+    cat("\n... nothing to do DROF file exists!\n")
+    
+  }
   
-  anom_avg <- mean( pr_stk / pr_stk_ref )
-  anom_std <- std( pr_stk / pr_stk_ref )
-  
-  anom_stk <- stack(layers=nlayers(pr_stk))
-  
-  spi <-  ( ( pr_stk - pr_stk_ref ) - anom_avg ) / anom_std 
-  # for (i in 1:12){anom_i <- ( ( pr_stk[[seq(i, nYears * 12, 12)]] - pr_stk_ref[[i]] ) - anom_avg ) / anom_std }
-  
-  # Write SPI (droi) and set years to time axis
-  writeRaster(spi, paste0(iDir, "/droi_tmp.nc"))
-  system(paste0("cdo settaxis,", iY, "-01-00,12:00:00,1year ", iDir, "/droi_tmp.nc ", oDroi))
-  unlink(paste0(iDir, "/droi_tmp.nc"))
-  
-  # Calculate frequence of SPI
-  spi_f <- spi
-  spi_f[which(spi_f[]<= thr)] <- 1/12
-  spi_f[which(spi_f[]> thr)] <- 0
-  
-  writeRaster(spi, paste0(iDir, "/drof_tmp_monthly.nc"))
-  system(paste0("cdo yearsum ", iDir, "/drof_tmp_monthly.nc ", iDir, "/drof_yearly.nc "))
-  system(paste0("cdo settaxis,", iY, "-01-00,12:00:00,1year ", iDir, "/drof_yearly.nc ", oDrof))
   
 }
 
