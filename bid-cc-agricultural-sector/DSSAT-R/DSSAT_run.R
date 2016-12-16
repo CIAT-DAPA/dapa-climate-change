72385eb7dbc1612b3126fa40409bb830c53bafb4
jramirezv <dawnpatrolmustaine@gmail.com>
2016-12-15 22:47:28 -0500

    changes to DSSAT_run.R for new Extraer.SoilDSSAT.R function

--- bid-cc-agricultural-sector/DSSAT-R/DSSAT_run.R
+++ bid-cc-agricultural-sector/DSSAT-R/DSSAT_run.R
@@ -130,7 +130,7 @@ run_dssat <- function(input, pixel, dir_dssat, dir_base) {
   CSMbatch(input$xfile$crop, input$xfile$name, input$xfile$bname) 
   
   ## Make Soil
-  Extraer.SoilDSSAT(pixel, getwd())
+  Extraer.SoilDSSAT(input, pixel, getwd())
   rewrite_soilfile("SOIL.SOL", "SOIL.SOL")
   
   ## Make Xfile 
--- bid-cc-agricultural-sector/DSSAT-R/Extraer.SoilDSSAT.R
+++ bid-cc-agricultural-sector/DSSAT-R/Extraer.SoilDSSAT.R
@@ -1,7 +1,7 @@
 #JRV extraer soil data from 30m grid
 #Dec 2016
 
-Extraer.SoilDSSAT <- function(pixel,path) {
+Extraer.SoilDSSAT <- function(input, pixel, path) {
   #position <- Codigo_identificadorSoil + 1   ## Where it coincides with the raster ID
   #posicion <- which(Soil_profile[, 1] == position)
   cell_id <- xy_Ref$CELL30M[which(round(xy_Ref$x,5) == round(input$climate$long[pixel],5) & round(xy_Ref$y,5) == round(input$climate$lat[pixel],5))]
@@ -18,11 +18,13 @@ Extraer.SoilDSSAT <- function(pixel,path) {
     Ref_for_Soil <- p_profile[Posicion_Pct,2][1]
     condicion <- which(Cod_Ref_and_Position[,1] == paste(Ref_for_Soil))
     
+    #if profile indeed exists
     if (length(condicion) >= 1) {
       Wise_Position <- Cod_Ref_and_Position[which(Cod_Ref_and_Position[, 1] == paste(Ref_for_Soil)), ]
       return(make_soilfile(in_data, wise[Wise_Position[, 2]:length(wise)], path))
     }
     
+    #else use generic
     if (length(condicion) == 0) {
       Wise_Position <- Cod_Ref_and_Position_Generic[which(Cod_Ref_and_Position_Generic[, 1] == paste(Ref_for_Soil)), 2]
       return(make_soilfile(in_data,Soil_Generic[Wise_Position:length(Soil_Generic)], path))
