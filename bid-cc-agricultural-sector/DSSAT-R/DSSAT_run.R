2cb00a2bd859f764e8267fd79553eb2773025700
jramirezv <dawnpatrolmustaine@gmail.com>
2016-12-15 22:41:34 -0500

    changes to DSSAT_run.R for new Extraer.SoilDSSAT.R function

--- bid-cc-agricultural-sector/DSSAT-R/DSSAT_run.R
+++ bid-cc-agricultural-sector/DSSAT-R/DSSAT_run.R
@@ -130,7 +130,7 @@ run_dssat <- function(input, pixel, dir_dssat, dir_base) {
   CSMbatch(input$xfile$crop, input$xfile$name, input$xfile$bname) 
   
   ## Make Soil
-  Extraer.SoilDSSAT(values[input$climate$id[pixel]],getwd())
+  Extraer.SoilDSSAT(input, getwd())
   rewrite_soilfile("SOIL.SOL", "SOIL.SOL")
   
   ## Make Xfile 
