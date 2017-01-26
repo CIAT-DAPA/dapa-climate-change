d62a30af1816d9e0ebcdd53ca3c70e8778727cd4
haachicanoy <harold22010@gmail.com>
2017-01-26 16:21:55 -0500

    Improvements in wheat run

--- bid-cc-agricultural-sector/DSSAT-R/_scriptsRuns/wheat_analytics.R
+++ bid-cc-agricultural-sector/DSSAT-R/_scriptsRuns/wheat_analytics.R
@@ -1,9 +1,19 @@
+# Wheat analytics
+# Implemented by: H. Achicanoy
+# CIAT, 2017
+
+# ------------------------------------------------------------------------------------------------------- #
+# Comparing yield results DIAGNOSTIC vs FINAL
+# ------------------------------------------------------------------------------------------------------- #
+
+# Put all stuff together (run in Linux)
 
 setwd('/home/jmesa/bid_reruns/')
 run_type <- c('diagnostic', 'final')
 cultivar <- c("Seri82BA", "TajanBA", "DonErnestoBA", "Gerek79BA", "HalconsnaBA", "BrigadierBA")
 
 library(dplyr)
+library(plyr)
 
 # Steps:
 
@@ -22,30 +32,35 @@ Run <- lapply(1:length(run_type), function(i){
     
   })
   
-  Run <- do.call(rbind_all, Run)
+  Run <- do.call(rbind, Run)
   return(Run)
   
 })
 
-Run <- do.call(rbind_all, Run)
+Run <- do.call(plyr::rbind.fill, Run)
 Run$Year <- as.numeric(substr(Run$SDAT, start = 1, stop = 4))
 
 write.csv(Run, '/home/jmesa/wheat_results.csv', row.names = F)
 
-# 3. Do some graphics that allow to figure out and explore differences produced by changes in second application date of fertilizer
+# 3. Explore differences produced by changes in second application date of fertilizer (run in Windows)
 
 options(warn = -1); options(scipen = 999)
-
 library(ggplot2)
 library(dplyr)
 library(tidyr)
 
 Run <- read.csv('wheat_results.csv')
 
+# ------------------------------------------------------------------------------------------------------- #
+# Dots plot
+# ------------------------------------------------------------------------------------------------------- #
+gg <- Run %>% ggplot(aes(x = as.numeric(Pixel), y = as.numeric(HWAH), group = as.factor(Year), colour = Run_type)) + geom_point()
+gg <- gg + facet_wrap(~ Cultivar) + theme_bw() + xlab('Pixel') + ylab('Yield (kg/ha)')
+ggsave(filename = 'Wheat_diagnostic_vs_final_ggcmi.png', plot = gg, width = 15, height = 10, units = 'in')
 
-Run %>% ggplot(aes(x = Year, y = HWAH, group = as.factor(Year), colour = as.factor(Pixel))) + geom_point() +
-  facet_grid(Run_type ~ Cultivar) + theme_bw()
-
+# ------------------------------------------------------------------------------------------------------- #
+# Quantile plot
+# ------------------------------------------------------------------------------------------------------- #
 quantileFun <- function(x){z <- stats::quantile(x, probs=0.05); return(z)}
 aux <- as.data.frame(dplyr::summarise(group_by(Run, Pixel, Cultivar, Run_type), quantileFun(HWAH)))
 names(aux)[4] <- "p05"
@@ -65,46 +80,11 @@ gg <- aux %>% ggplot(aes(x = Pixel, y = p50, ymin = p05, ymax = p90, colour = Ru
 gg <- gg + geom_linerange(position = position_dodge(width = c(0.6, 0.4)), size = 1, alpha = 0.5)
 gg <- gg + geom_point(aes(color = Run_type, shape = Run_type), position = position_dodge(width = c(0.6, 0.4)), size = 3)
 gg <- gg + facet_wrap(~ Cultivar) + theme_bw() + xlab('Pixel') + ylab('Yield (kg/ha)')
-gg
-ggsave(filename = 'Wheat_diagnostic_vs_final_90interval.pdf', plot = gg, width = 15, height = 10, units = 'in')
-system(paste("convert -verbose -density 300 Wheat_diagnostic_vs_final_90interval.pdf -quality 100 -sharpen 0x1.0 -alpha off Wheat_diagnostic_vs_final_90interval.png", sep=""), wait=TRUE)
+ggsave(filename = 'Wheat_diagnostic_vs_final_90interval_ggcmi.png', plot = gg, width = 15, height = 10, units = 'in')
 
-ggplot(dat, aes(x = Treatment, y = Meas, ymin = Meas - SD/2, ymax = Meas + SD/2)) +
-  geom_linerange(aes(color = Temp), position=position_dodge(width=c(0.6,0.4)), size = 1, alpha = 0.5) +
-  geom_point(aes(color = Temp, shape = Temp), position=position_dodge(width=c(0.6,0.4)), size = 3) +
-  theme_bw()
-
-plot(x = Run$Year[Run$Pixel == 1 & Run$Cultivar == "Seri82BA" & Run$Run_type == "diagnostic"],
-     y = Run$HWAH[Run$Pixel == 1 & Run$Cultivar == "Seri82BA" & Run$Run_type == "diagnostic"], ty = 'l')
-
-hist(Run$Year[Run$Pixel == 1 & Run$Cultivar == "Seri82BA" & Run$Run_type == "diagnostic"])
-median(Run$Year[Run$Pixel == 1 & Run$Cultivar == "Seri82BA" & Run$Run_type == "diagnostic"])
-
-# To fix
-gg <- Run %>% ggplot(aes(x = as.numeric(Pixel), y = as.numeric(HWAH), group = as.factor(Year), colour = Run_type)) + geom_point() +
-  facet_wrap(~ Cultivar) + theme_bw() + xlab('Pixel') + ylab('Yield (kg/ha)')
-gg
-ggsave(filename = 'Wheat_diagnostic_vs_final.pdf', plot = gg, width = 12, height = 8, units = 'in')
-system(paste("convert -verbose -density 300 Wheat_diagnostic_vs_final.pdf -quality 100 -sharpen 0x1.0 -alpha off Wheat_diagnostic_vs_final.png", sep=""), wait=TRUE)
-
-dat1<-cbind(dat,aux=rep(1,length(dat[,1]))) 
-dat1<-within(dat1, {aux = unlist(by(aux,Treatment,cumsum))})
-dat1$aux<-dat1$aux+as.numeric(dat1$Treatment)*10
-ggplot(dat1, aes( x=aux, y = Meas, ymin = Meas - SD/2, ymax = Meas + SD/2)) +
-  geom_linerange(aes(color = Temp), size = 1, alpha = 0.5) +geom_point(aes(color = Temp, shape = Temp))+
-  scale_x_continuous("Treatment",breaks=c(13.5,23.5), labels=c("A","B")) + # here you define coordinates for A and B 
-  theme_bw()
-
-
-
-gg <- ggplot(YieldMatrix, aes(x = year, y = yield, group = run_type, colour = run_type)) + geom_line()
-gg <- gg + xlab('Year') + ylab('Yield') + theme_bw()
-gg
-
-
-# --------------------------------------------------------------------------------------------------------------------- #
-# Plotting Mirca dates
-# --------------------------------------------------------------------------------------------------------------------- #
+# ------------------------------------------------------------------------------------------------------- #
+# Load crop management matrix
+# ------------------------------------------------------------------------------------------------------- #
 cultivar <- 1
 cul_list <- data.frame(CID=1:6,dsid=c("IB0010","IB0013","IB0016","IB0028","IB0022","IB0026"),
                        culname=c("Seri82BA","TajanBA","DonErnestoBA","Gerek79BA","HalconsnaBA","BrigadierBA"))
@@ -122,38 +102,56 @@ assign("crop_mgmt", get(paste("crop_",sys_type,sep="")))
 
 rm(list=setdiff(ls(), "crop_mgmt"))
 
+crop_mgmt$Pixel <- 1:nrow(crop_mgmt)
+
+# ------------------------------------------------------------------------------------------------------- #
+# MIRCA planting dates
+# ------------------------------------------------------------------------------------------------------- #
+latam_map <- ggplot() + geom_polygon(data = latam, aes(x = long, y = lat, group = group), fill = "black", colour = "grey90", alpha = 1)
+latam_map <- latam_map + theme_dark()
+latam_map <- latam_map + geom_point(data = crop_mgmt, aes(x = x, y = y, color = as.factor(mirca.start)), alpha = 1, size = 3)
+latam_map <- latam_map + xlab('Longitude') + ylab('Latitude') + guides(color = guide_legend(title = 'Mirca start'))
+latam_map <- latam_map + coord_equal(ratio=1)
+ggsave(filename = 'mirca_start.png', plot = latam_map, width = 10, height = 10, units = "in")
+
+# ------------------------------------------------------------------------------------------------------- #
+# Update planting dates using GGCMI data
+# ------------------------------------------------------------------------------------------------------- #
+
 # Load GGCMI data
 library(ncdf4)
 library(raster)
 
 wd_dir <- 'D:/ToBackup/Modelling/bid-cc-agricultural-sector/GGCMI_data/GGCMI'
-ggcmi <- raster::raster(paste(wd_dir, "/Wheat_ir_growing_season_dates_v1.25.nc4", sep = ""))
+library(ncdf4)
+library(raster)
+ggcmi <- brick(paste(wd_dir, "/Wheat_ir_growing_season_dates_v1.25.nc4", sep = ""), varname="planting day")
+ggcmi <- ggcmi[[1]]
 ggcmi[which(ggcmi[] == -99)] <- NA
 
-ggcmi_dates <- raster::extract(x = ggcmi, y = crop_mgmt[, c('x', 'y')])
-plot(x = crop_mgmt$mirca.start, y = ggcmi_dates, ty = "p", pch = 20, xlab = "Mirca start date", ylab = "GGCMI start date")
-abline(0, 1)
+planting_dates <- raster::extract(x = ggcmi, y = crop_mgmt[, c('x', 'y')])
 
 crop_mgmt2 <- crop_mgmt
-crop_mgmt$mirca.start <- ggcmi_dates
+crop_mgmt$mirca.start <- round(planting_dates, 0)
 
 saveRDS(object = crop_mgmt, file = 'D:/ToBackup/Modelling/bid-cc-agricultural-sector/crop_mgmt.rds')
 
-
-library(class)
-
-train <- crop_mgmt[!is.na(crop_mgmt$mirca.start), c("x", "y")]
-test <- crop_mgmt[is.na(crop_mgmt$mirca.start), c("x", "y")]
-
-train_labels <- crop_mgmt$mirca.start[!is.na(crop_mgmt$mirca.start)]
-test_labels <- crop_mgmt$mirca.start[is.na(crop_mgmt$mirca.start)]
-
-knn(train = train, test = test, cl = train_labels, k = 50)
+# library(class)
+# 
+# train <- crop_mgmt[!is.na(crop_mgmt$mirca.start), c("x", "y")]
+# test <- crop_mgmt[is.na(crop_mgmt$mirca.start), c("x", "y")]
+# 
+# train_labels <- crop_mgmt$mirca.start[!is.na(crop_mgmt$mirca.start)]
+# test_labels <- crop_mgmt$mirca.start[is.na(crop_mgmt$mirca.start)]
+# 
+# knn(train = train, test = test, cl = train_labels, k = 50)
 
 library(rgdal)
 latam <- readOGR("//dapadfs/workspace_cluster_3/bid-cc-agricultural-sector/03-Map_LatinAmerica/Latino_America.shp", layer="Latino_America")
 
-# GGCMI start dates
+# ------------------------------------------------------------------------------------------------------- #
+# GGCMI planting dates
+# ------------------------------------------------------------------------------------------------------- #
 latam_map <- ggplot() + geom_polygon(data = latam, aes(x = long, y = lat, group = group), fill = "black", colour = "grey90", alpha = 1)
 latam_map <- latam_map + theme_dark()
 latam_map <- latam_map + geom_point(data = crop_mgmt, aes(x = x, y = y, color = as.factor(mirca.start)), alpha = 1, size = 3)
@@ -162,23 +160,20 @@ latam_map <- latam_map + coord_equal(ratio=1)
 latam_map
 ggsave(filename = 'ggcmi_start.png', plot = latam_map, width = 10, height = 10, units = "in")
 
-# MIRCA start dates
+# ------------------------------------------------------------------------------------------------------- #
+# Low yield pixels 200-300
+# ------------------------------------------------------------------------------------------------------- #
 latam_map <- ggplot() + geom_polygon(data = latam, aes(x = long, y = lat, group = group), fill = "black", colour = "grey90", alpha = 1)
 latam_map <- latam_map + theme_dark()
-latam_map <- latam_map + geom_point(data = crop_mgmt, aes(x = x, y = y, color = as.factor(mirca.start)), alpha = 1, size = 3)
-latam_map <- latam_map + xlab('Longitude') + ylab('Latitude') + guides(color = guide_legend(title = 'Mirca start'))
+latam_map <- latam_map + geom_point(data = crop_mgmt[200:300,], aes(x = x, y = y, color = "red"), alpha = 1, size = 3)
+latam_map <- latam_map + xlab('Longitude') + ylab('Latitude') + guides(color = guide_legend(title = 'Low yields'))
 latam_map <- latam_map + coord_equal(ratio=1)
-ggsave(filename = 'mirca_start.png', plot = latam_map, width = 10, height = 10, units = "in")
+latam_map
+ggsave(filename = 'low_yields.png', plot = latam_map, width = 10, height = 10, units = "in")
 
-latam_map <- ggplot() + geom_polygon(data = latam, aes(x = long, y = lat, group = group), fill = "black", colour = "grey90", alpha = 1)
-latam_map <- latam_map + theme_dark()
-latam_map <- latam_map + geom_point(data = crop_mgmt, aes(x = x, y = y, color = as.factor(mirca.end)), alpha = 1, size = 3)
-latam_map <- latam_map + xlab('Longitude') + ylab('Latitude') + guides(color = guide_legend(title = 'Mirca end'))
-latam_map <- latam_map + coord_equal(ratio=1)
-ggsave(filename = 'mirca_end.png', plot = latam_map, width = 10, height = 10, units = "in")
-
+# ------------------------------------------------------------------------------------------------------- #
 # Relation: MIRCA dates vs yield|temperature
-
+# ------------------------------------------------------------------------------------------------------- #
 crop_mgmt$Pixel <- 1:nrow(crop_mgmt)
 
 seri82ba <- Run[Run$Cultivar == "Seri82BA" & Run$Run_type == "diagnostic",]; rownames(seri82ba) <- 1:nrow(seri82ba)
@@ -197,3 +192,104 @@ gg <- gg + theme_bw() + xlab('Latitude') + ylab('Maximum temperature') + scale_c
 gg <- gg + guides(size = guide_legend(title = 'Mirca start date'))
 gg <- gg + guides(colour = guide_legend(title = 'Mirca start date'))
 ggsave(filename = 'latitude_vs_tmax_by_mircastart.png', plot = gg, width = 10, height = 10, units = "in")
+
+# ------------------------------------------------------------------------------------------------------- #
+# Comparison between MIRCA and GGCMI
+# ------------------------------------------------------------------------------------------------------- #
+
+g <- gc(); rm(list = ls())
+options(warn = -1); options(scipen = 999)
+library(plyr)
+library(ggplot2)
+library(dplyr)
+
+wk_dir <- "D:/ToBackup/Modelling/bid-cc-agricultural-sector/Results/Historical"
+setwd(wk_dir)
+allResults <- lapply(list.files(getwd()), function(x){
+  df <- read.csv(x)
+  df$Source <- gsub(pattern = ".csv", replacement = "", x = unlist(strsplit(x = basename(x), split = "_"))[3])
+  return(df)
+}); allResults <- do.call(rbind.fill, allResults)
+
+# ------------------------------------------------------------------------------------------------------- #
+# Quantile plots
+# ------------------------------------------------------------------------------------------------------- #
+quantileFun <- function(x){z <- stats::quantile(x, probs=0.05); return(z)}
+aux <- as.data.frame(dplyr::summarise(group_by(allResults, Pixel, Cultivar, Run_type, Source), quantileFun(HWAH)))
+names(aux)[5] <- "p05"
+
+quantileFun <- function(x){z <- stats::quantile(x, probs=0.50); return(z)}
+aux1 <- as.data.frame(dplyr::summarise(group_by(allResults, Pixel, Cultivar, Run_type, Source), quantileFun(HWAH)))
+names(aux1)[5] <- "p50"
+
+quantileFun <- function(x){z <- stats::quantile(x, probs=0.90); return(z)}
+aux2 <- as.data.frame(dplyr::summarise(group_by(allResults, Pixel, Cultivar, Run_type, Source), quantileFun(HWAH)))
+names(aux2)[5] <- "p90"
+
+aux <- merge(aux, aux1, by = c("Pixel", "Cultivar", "Run_type", "Source")); rm(aux1)
+aux <- merge(aux, aux2, by = c("Pixel", "Cultivar", "Run_type", "Source")); rm(aux2)
+
+# ------------------------------------------------------------------------------------------------------- #
+# Diagnostic
+# ------------------------------------------------------------------------------------------------------- #
+gg <- aux[aux$Run_type == "diagnostic", ] %>% ggplot(aes(x = Pixel, y = p50, ymin = p05, ymax = p90, colour = Source))
+gg <- gg + geom_linerange(position = position_dodge(width = c(0.6, 0.4)), size = 1, alpha = 0.5)
+gg <- gg + geom_point(aes(color = Source, shape = Source), position = position_dodge(width = c(0.6, 0.4)), size = 3)
+gg <- gg + facet_wrap(~ Cultivar) + theme_bw() + xlab('Pixel') + ylab('Yield (kg/ha)')
+ggsave(filename = 'Wheat_mirca_vs_ggcmi_90interval_diagnostic.png', plot = gg, width = 15, height = 10, units = 'in')
+
+# ------------------------------------------------------------------------------------------------------- #
+# Final
+# ------------------------------------------------------------------------------------------------------- #
+gg <- aux[aux$Run_type == "final", ] %>% ggplot(aes(x = Pixel, y = p50, ymin = p05, ymax = p90, colour = Source))
+gg <- gg + geom_linerange(position = position_dodge(width = c(0.6, 0.4)), size = 1, alpha = 0.5)
+gg <- gg + geom_point(aes(color = Source, shape = Source), position = position_dodge(width = c(0.6, 0.4)), size = 3)
+gg <- gg + facet_wrap(~ Cultivar) + theme_bw() + xlab('Pixel') + ylab('Yield (kg/ha)')
+ggsave(filename = 'Wheat_mirca_vs_ggcmi_90interval_final.png', plot = gg, width = 15, height = 10, units = 'in')
+
+# ------------------------------------------------------------------------------------------------------- #
+# Mean yield map
+# ------------------------------------------------------------------------------------------------------- #
+
+library(dplyr)
+library(ncdf4)
+library(raster)
+library(rgdal)
+library(ggplot2)
+library(scales)
+
+wd_dir <- 'D:/ToBackup/Modelling/bid-cc-agricultural-sector/GGCMI_data/GGCMI'
+ggcmi <- brick(paste(wd_dir, "/Wheat_ir_growing_season_dates_v1.25.nc4", sep = ""), varname="planting day")
+ggcmi <- ggcmi[[1]]; rm(wd_dir)
+
+latam <- readOGR("//dapadfs/workspace_cluster_3/bid-cc-agricultural-sector/03-Map_LatinAmerica/Latino_America.shp", layer="Latino_America")
+ggcmi <- raster::crop(x = ggcmi, y = extent(latam))
+ggcmi[] <- NA
+
+Run <- read.csv("D:/ToBackup/Modelling/bid-cc-agricultural-sector/Results/Historical/wheat_results_ggcmi.csv")
+Yld <- as.data.frame(dplyr::summarise(group_by(Run, Pixel, Cultivar, Run_type), mean(HWAH)))
+names(Yld)[4] <- "Mean_yield"
+
+Yld <- merge(x = Yld, y = crop_mgmt, by = "Pixel")
+
+cultivarList <- c("Seri82BA", "TajanBA", "DonErnestoBA", "Gerek79BA", "HalconsnaBA", "BrigadierBA")
+run_typeList <- c("diagnostic", "final")
+lapply(1:length(cultivarList), function(i){
+  
+  lapply(1:length(run_typeList), function(j){
+    
+    cells <- cellFromXY(object = ggcmi, xy = cbind(Yld$x[Yld$Cultivar == cultivarList[i] & Yld$Run_type == run_typeList[j]], Yld$y[Yld$Cultivar == cultivarList[i] & Yld$Run_type == run_typeList[j]]))
+    ggcmi[cells] <- Yld$Mean_yield[Yld$Cultivar == cultivarList[i] & Yld$Run_type == run_typeList[j]]
+    
+    ggcmi.p <- data.frame(rasterToPoints(ggcmi))
+    
+    latam_map <- ggplot() + geom_polygon(data = latam, aes(x = long, y = lat, group = group), fill = "black", colour = "grey90", alpha = 1)
+    latam_map <- latam_map + theme_dark()
+    latam_map <- latam_map + geom_raster(data = ggcmi.p, aes(x = x, y = y, fill = layer))
+    latam_map <- latam_map + coord_equal(ratio = 1) + scale_fill_gradientn(colours = c("darkgoldenrod4", "yellow", "forestgreen"), values = rescale(c(0, 5000, 10000, 15000, 20000)))
+    latam_map <- latam_map + guides(fill = guide_legend(title = "Yield (kg/ha)"))
+    ggsave(filename = paste("mean_yield_map_", cultivarList[i], "_", run_typeList[j], ".png", sep = ""), plot = latam_map, width = 10, height = 10, units = "in")
+    
+  })
+  
+})
