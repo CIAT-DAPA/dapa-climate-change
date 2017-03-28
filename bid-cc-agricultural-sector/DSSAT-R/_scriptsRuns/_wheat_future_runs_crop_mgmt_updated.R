b9d659652c6e24477a89d2a3b33f3887e4f65b01
haachicanoy <harold22010@gmail.com>
2017-03-28 07:31:32 -0500

    Wheat done

--- bid-cc-agricultural-sector/DSSAT-R/_scriptsRuns/_wheat_future_runs_crop_mgmt_updated.R
+++ bid-cc-agricultural-sector/DSSAT-R/_scriptsRuns/_wheat_future_runs_crop_mgmt_updated.R
@@ -30,7 +30,7 @@ cleanup_all <- F
 ##############################################################################
 
 # Iterate GCM's
-for (gcm_i in 4) { # 1:length(modelos)
+for (gcm_i in 1:length(modelos)) {
   
   cat(paste("Processing of: ", modelos[gcm_i], "\n", sep = ""))
   
