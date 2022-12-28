64f371d85763f51787f87bfd19f9d31c91c0581a
Carlos Eduardo Navarro Racines <c.e.navarro@cgiar.org>
2022-12-28 10:39:17 -0500

    descarga era5

---
+++ data-processing-R/descarga_era5.R
@@ -0,0 +1,33 @@
+devtools::install_github("agrdatasci/ag5Tools", build_vignettes = TRUE)
+
+library(ag5Tools)
+
+## Download Solar Radiation ##
+
+ag5Tools::ag5_download(variable = "solar_radiation_flux",
+                       #statistic = "night_time_minimum",
+                       day = "all",
+                       month = "all",
+                       year = 2021,
+                       path = "D:/cenavarro/Workspace/era5/" # Folder to save the data
+)
+
+
+## Download Tmin ##
+
+ag5Tools::ag5_download(variable = "2m_temperature",
+                       statistic = "24_hour_minimum",
+                       day = "all",
+                       month = "all",
+                       year = 2021,
+                       path = "D:/cenavarro/Workspace/era5/" # Folder to save the data
+)
+
+## Download Tmax ##
+ag5Tools::ag5_download(variable = "2m_temperature",
+                       statistic = "24_hour_maximum",
+                       day = "all",
+                       month = "all",
+                       year = 2021,
+                       path = "D:/cenavarro/Workspace/era5/" # Folder to save the data
+)
